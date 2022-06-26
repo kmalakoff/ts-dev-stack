0942d350425eaffab4040162aa483b5e0bb17f2b
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-26 00:45:46 -0700

    Added .tsx support

--- test/data/fetch-http-message/src/index.tsx
+++ test/data/fetch-http-message/src/index.tsx
@@ -1,22 +1,17 @@
-import blobToString from "./blobToString";
+import blobToString from './blobToString';
 
 export interface HeadersObject {
   [key: string]: string;
 }
 
 // https://github.com/node-fetch/node-fetch/blob/main/%40types/index.d.ts
-export type HeadersInit =
-  | Headers
-  | HeadersObject
-  | Record<string, string>
-  | Iterable<readonly [string, string]>
-  | Iterable<Iterable<string>>;
-const hasHeaders = typeof Headers !== "undefined";
+export type HeadersInit = Headers | HeadersObject | Record<string, string> | Iterable<readonly [string, string]> | Iterable<Iterable<string>>;
+const hasHeaders = typeof Headers !== 'undefined';
 
 export type BodyInit = Blob | Buffer | URLSearchParams | string;
-const hasBlob = typeof Blob !== "undefined";
-const hasBuffer = typeof Buffer !== "undefined";
-const hasURLSearchParams = typeof URLSearchParams !== "undefined";
+const hasBlob = typeof Blob !== 'undefined';
+const hasBuffer = typeof Buffer !== 'undefined';
+const hasURLSearchParams = typeof URLSearchParams !== 'undefined';
 
 // export type RequestRedirect = 'error' | 'follow' | 'manual';
 export interface RequestInit {
@@ -35,7 +30,7 @@ export interface Request {
 }
 
 function isRequest(object) {
-  return typeof object === "object";
+  return typeof object === 'object';
 }
 
 /**
@@ -46,7 +41,7 @@ function isRequest(object) {
  * @returns The http message string
  */
 export default function fetchHttpMessage(input: RequestInfo, init?: RequestInit): string {
-  if (input === undefined) throw new Error("Input is expected");
+  if (input === undefined) throw new Error('Input is expected');
   if (init === undefined) init = {};
 
   let url;
@@ -56,7 +51,7 @@ export default function fetchHttpMessage(input: RequestInfo, init?: RequestInit)
     input = {} as Request;
   }
 
-  let method = init.method || (input as Request).method || "GET";
+  let method = init.method || (input as Request).method || 'GET';
   method = method.toUpperCase();
   const lines = [`${method} ${url} HTTP/1.1`];
 
@@ -73,23 +68,23 @@ export default function fetchHttpMessage(input: RequestInfo, init?: RequestInit)
 
   const body = init.body;
   if (body !== undefined) {
-    if (~["GET", "HEAD"].indexOf(method)) throw new Error(`Option body not valid with method ${method}`);
+    if (~['GET', 'HEAD'].indexOf(method)) throw new Error(`Option body not valid with method ${method}`);
     /* c8 ignore start */
     if (hasBlob && body instanceof Blob) {
-      lines.push("");
+      lines.push('');
       lines.push(blobToString(body));
     } else if (
       /* c8 ignore stop */
-      typeof body === "string" ||
+      typeof body === 'string' ||
       body instanceof String ||
       /* c8 ignore start */
       (hasBuffer && body instanceof Buffer) ||
       (hasURLSearchParams && body instanceof URLSearchParams)
       /* c8 ignore stop */
     ) {
-      lines.push("");
+      lines.push('');
       lines.push(body.toString());
-    } else throw new Error("Option body should be convertible to a string");
+    } else throw new Error('Option body should be convertible to a string');
   }
-  return lines.join("\r\n");
+  return lines.join('\r\n');
 }
---
+++ test/spec/lib.test.js
@@ -0,0 +1,116 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+var assert = require('assert');
+var path = require('path');
+
+var LIB = path.join(__dirname, '..', '..', 'lib');
+var DATA_DIR = path.resolve(__dirname, '..', 'data', 'fetch-http-message');
+
+var major = +process.versions.node.split('.')[0];
+
+describe('lib', function () {
+  describe('happy path', function () {
+    major < 14 ||
+      it('build:dist', function (done) {
+        process.chdir(DATA_DIR);
+        require(path.join(LIB, 'build'))(['src/index.tsx'], {}, function (err) {
+          assert.ok(!err);
+          done();
+        });
+      });
+
+    it('test:dist', function (done) {
+      process.chdir(DATA_DIR);
+      require(path.join(LIB, 'test'))([], {}, function (err) {
+        assert.ok(!err);
+        done();
+      });
+    });
+  });
+  if (major < 14) return;
+
+  // TODO: add tests
+  it('link', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'link'))([], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+
+  it('build', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'build'))(['src/index.tsx'], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+
+  it('docs', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'build', 'docs'))(['src/index.tsx'], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+
+  it('coverage:node', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'quality', 'c8'))([], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+
+  // eslint-disable-next-line mocha/no-skipped-tests
+  it.skip('deploy', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'deploy'))([], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+  it('format', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'quality', 'format'))([], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+  it('lint', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'quality', 'lint'))([], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+  it('test', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'test'))([], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+
+  it('test:engines', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'test', 'engines'))([], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+  it('test:browser', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'test', 'karma'))([], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+  // eslint-disable-next-line mocha/no-skipped-tests
+  it.skip('version', function (done) {
+    process.chdir(DATA_DIR);
+    require(path.join(LIB, 'deploy', 'version'))([], {}, function (err) {
+      assert.ok(!err);
+      done();
+    });
+  });
+});
