c25a3082a3e6c43efe81dfaf802522b78a51a813
Kevin Malakoff <kmalakoff@gmail.com>
2022-06-26 15:28:15 -0700

    Added np flags for testing

--- test/data/react-ref-boundary/babel.config.cjs
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const babelConfig = require('./node_modules/ts-dev-stack/.babelrc.json');
-const testConfig = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
-module.exports = process.env.BABEL_ENV === 'test' ? testConfig : babelConfig;
