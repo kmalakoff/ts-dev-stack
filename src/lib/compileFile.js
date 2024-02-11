f22ce97f2020fd4b6350759b0573bbe928b9f16f
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-11 08:42:25 -0800

    Modern formatting

--- src/lib/compileFile.js
+++ src/lib/compileFile.js
@@ -1,14 +1,14 @@
-var path = require('path');
-var fs = require('fs');
-var Queue = require('queue-cb');
-var once = require('call-once-fn');
-var assign = require('just-extend');
-var mkdirp = require('mkdirp');
+const path = require('path');
+const fs = require('fs');
+const Queue = require('queue-cb');
+const once = require('call-once-fn');
+const assign = require('just-extend');
+const mkdirp = require('mkdirp');
 
-var transformSync = require('ts-swc-loaders/lib/transformSync.js');
+const transformSync = require('ts-swc-loaders/lib/transformSync.js');
 
 // https://github.com/vercel/next.js/blob/20b63e13ab2631d6043277895d373aa31a1b327c/packages/next/taskfile-swc.js#L118-L125
-var interopClientDefaultExport = [
+const interopClientDefaultExport = [
   '',
   "if ((typeof exports.default === 'function' || (typeof exports.default === 'object' && exports.default !== null)) && typeof exports.default.__esModule === 'undefined') {",
   "  Object.defineProperty(exports.default, '__esModule', { value: true });",
@@ -18,12 +18,12 @@ var interopClientDefaultExport = [
 ].join('\n');
 
 module.exports = function compileFile(entry, options, callback) {
-  fs.readFile(entry.fullPath, 'utf8', function (err, contents) {
+  fs.readFile(entry.fullPath, 'utf8', (err, contents) => {
     if (err) return callback(err);
     callback = once(callback);
 
     try {
-      var config = options.config;
+      let config = options.config;
 
       // overrides for cjs
       if (options.type === 'cjs') {
@@ -34,9 +34,9 @@ module.exports = function compileFile(entry, options, callback) {
         config.config.compilerOptions.target = 'ES5';
       }
 
-      var output = transformSync(contents, entry.basename, config);
-      var relname = entry.path.replace(/\.[^/.]+$/, '');
-      var ext = options.type === 'esm' ? '.mjs' : '.js';
+      const output = transformSync(contents, entry.basename, config);
+      const relname = entry.path.replace(/\.[^/.]+$/, '');
+      const ext = options.type === 'esm' ? '.mjs' : '.js';
 
       // patch .mjs imports
       if (options.type === 'esm') {
@@ -49,10 +49,10 @@ module.exports = function compileFile(entry, options, callback) {
         output.code += interopClientDefaultExport;
       }
 
-      mkdirp(path.dirname(path.join(options.dest, relname + ext)), function () {
-        var outQueue = new Queue();
+      mkdirp(path.dirname(path.join(options.dest, relname + ext)), () => {
+        const outQueue = new Queue();
         outQueue.defer(fs.writeFile.bind(null, path.join(options.dest, relname + ext), output.code, 'utf8'));
-        !options.sourceMaps || outQueue.defer(fs.writeFile.bind(null, path.join(options.dest, relname + ext + '.map'), output.map, 'utf8'));
+        !options.sourceMaps || outQueue.defer(fs.writeFile.bind(null, path.join(options.dest, `${relname + ext}.map`), output.map, 'utf8'));
         outQueue.await(callback);
       });
     } catch (err) {
