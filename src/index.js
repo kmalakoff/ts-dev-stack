03233c4fafc1e9f5e65c07054357bdedb97eb228
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-11 02:50:05 -0800

    Build ts-dev-script with ts-dev-script

---
+++ src/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  build: require('./build'),
+  coverage: require('./test/c8'),
+  deploy: require('./deploy'),
+  docs: require('./docs'),
+  format: require('./quality/format'),
+  link: require('./link'),
+  test: require('./test'),
+  'test:node': require('./test/mocha'),
+  'test:browser': require('./test/karma'),
+  unlink: require('./unlink'),
+  version: require('./deploy/version'),
+};
