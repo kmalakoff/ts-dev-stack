3c7116975bb2b456cc9c47a639b11202dedb2a60
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-11 16:30:26 -0800

    Upgrade ts-swc-loader

--- src/unlink.js
+++ src/unlink.js
@@ -5,7 +5,7 @@ module.exports = function linkCmd(_args, options, cb) {
   const cwd = options.cwd || process.cwd();
   try {
     const pkg = require(path.resolve(cwd, 'package.json'));
-    const installPath = path.resolve(cwd, 'node_modules', pkg.name);
+    const installPath = options.installPath || path.resolve(cwd, 'node_modules', pkg.name);
     unlink(installPath, cb);
   } catch (err) {
     return cb(err);
