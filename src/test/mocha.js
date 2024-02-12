9ccc34822c8b38ab1cea30ab9205003b37a94311
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-11 16:30:26 -0800

    Upgrade ts-swc-loader

--- src/test/mocha.js
+++ src/test/mocha.js
@@ -1,21 +1,23 @@
+const _path = require('path');
 const Queue = require('queue-cb');
+const spawnArgs = require('ts-swc-loaders/lib/spawnArgs.js');
 
 const link = require('../link');
 const spawn = require('../lib/spawn');
 
 const major = +process.versions.node.split('.')[0];
+const type = major >= 12 ? 'module' : 'commonjs';
 
 module.exports = function mocha(args, options, cb) {
   link(args, options, (_err, restore) => {
     const queue = new Queue(1);
     queue.defer((cb) => {
-      const tests = args.length ? args[0] : 'test/**/*.test.*';
+      const tests = args && args.length ? args[0] : 'test/**/*.test.*';
       const mochaOptions = options.timeout ? ['--timeout', options.timeout] : [];
-      if (major < 12) {
-        spawn('mocha-compat', mochaOptions.concat(['--require', 'ts-swc-loaders', '--watch-extensions', 'ts,tsx', tests]), {}, cb);
-      } else {
-        spawn('mocha', mochaOptions.concat(['--watch-extensions', 'ts,tsx,mjs', tests]), { env: { NODE_OPTIONS: '--loader ts-swc-loaders' } }, cb);
-      }
+      const mocha = major >= 12 ? 'mocha' : 'mocha-compat';
+      const cmd = require.resolve(`${mocha}/bin/_${mocha}`);
+      const argsSpawn = spawnArgs(type, cmd, mochaOptions.concat(['--watch-extensions', 'ts,tsx', tests]), {});
+      spawn(argsSpawn[0], argsSpawn[1], argsSpawn[2], cb);
     });
     queue.await((err) => {
       restore((err2) => {
