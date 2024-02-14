6e56581adf8bd67347dd1d853427a03361f02a32
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-14 03:30:14 -0800

    Update to ts-dev-stack

--- src/build/cjs.js
+++ src/build/cjs.js
@@ -1,18 +1,18 @@
-var fs = require('fs');
-var path = require('path');
-var rimraf = require('rimraf');
-var Queue = require('queue-cb');
-var assign = require('just-extend');
-var compileDirectory = require('../lib/compileDirectory');
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const Queue = require('queue-cb');
+const assign = require('just-extend');
+const compileDirectory = require('../lib/compileDirectory');
 
 module.exports = function cjs(_args, options, cb) {
-  var cwd = options.cwd || process.cwd();
+  const cwd = options.cwd || process.cwd();
   options = assign({}, options);
   options.type = 'cjs';
   options.sourceMaps = true;
   options.dest = path.join(cwd, 'dist', 'cjs');
-  rimraf(options.dest, function () {
-    var queue = new Queue(1);
+  rimraf(options.dest, () => {
+    const queue = new Queue(1);
     queue.defer(compileDirectory.bind(null, options));
     queue.defer(fs.writeFile.bind(null, path.join(options.dest, 'package.json'), '{"type":"commonjs"}'));
     queue.await(cb);
