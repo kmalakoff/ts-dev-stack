bc44c525ab914ac645b8b5096a5be01ee6d854aa
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-15 23:15:24 -0800

    Upgrade rollup

---
+++ src/rollup-swc/swc.mjs
@@ -0,0 +1,18 @@
+import path from 'path';
+import getTS from 'get-tsconfig-compat';
+import { transformSync, createMatcher } from 'ts-swc-loaders';
+
+const config = getTS.getTsconfig(path.resolve(process.cwd(), 'tsconfig.json'));
+config.config.compilerOptions.target = 'ES5';
+const matcher = createMatcher(config);
+
+export default function swcPlugin() {
+
+  return {
+    name: 'swc',
+    transform(contents, filename) {
+      if (!matcher(filename)) return null;
+      return transformSync(contents, filename, config);
+    },
+  };
+};
