088b1555f5f9bb0cc55effad0ddd2db7bf08035d
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-15 17:19:44 -0800

    Remove loader warnings

--- src/test/c8.js
+++ src/test/c8.js
@@ -1,12 +1,16 @@
 const path = require('path');
 const rimraf = require('rimraf');
 const Queue = require('queue-cb');
+// const { spawnArgs } = require('ts-swc-loaders');
 
 const link = require('../link');
 const spawn = require('../lib/spawn');
 
-module.exports = function c8(_args, _options, cb) {
-  link(_args, _options, (err, restore) => {
+// const major = +process.versions.node.split('.')[0];
+// const type = major >= 12 ? 'module' : 'commonjs';
+
+module.exports = function c8(_args, options, cb) {
+  link(_args, options, (err, restore) => {
     if (err) return cb(err);
 
     const queue = new Queue(1);
@@ -16,10 +20,13 @@ module.exports = function c8(_args, _options, cb) {
       });
     });
     queue.defer((cb) => {
-      let args = ['--config', path.join(__dirname, '..', '..', '..', 'assets', 'c8rc.json')];
-      args.push('mocha');
+      // TODO: get spawn working for c8
+      const cmd = path.resolve('./node_modules/.bin/c8');
+      let args = ['--no-warnings=ExperimentalWarning', cmd, '--config', path.join(__dirname, '..', '..', '..', 'assets', 'c8rc.json'), 'mocha', '--watch-extensions', 'ts,tsx'];
       args = args.concat(_args.length ? _args.slice(-1) : ['test/unit/*.test.*']);
-      spawn('c8', args, { env: { NODE_OPTIONS: '--loader ts-swc-loaders' } }, cb);
+      spawn('node', args, { env: { NODE_OPTIONS: '--no-warnings=ExperimentalWarning --loader ts-swc-loaders' } }, cb);
+      // const argsSpawn = spawnArgs(type, cmd, args, {});
+      // spawn(argsSpawn[0], argsSpawn[1], argsSpawn[2], cb);
     });
     queue.await((err) => {
       restore((err2) => {
