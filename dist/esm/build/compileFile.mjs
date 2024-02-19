5b587a00bdb12443f204c06131c6d8f7917d5a1f
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-18 21:32:14 -0800

    Upgrade dependencies

--- dist/esm/build/compileFile.mjs
+++ dist/esm/build/compileFile.mjs
@@ -47,7 +47,7 @@ function makeReplacements(code, regex, extensions, extension) {
     return code;
 }
 // https://github.com/vercel/next.js/blob/20b63e13ab2631d6043277895d373aa31a1b327c/packages/next/taskfile-swc.js#L118-L125
-const interopClientDefaultExport = '/* CJS INTEROP */ if (exports.__esModule && exports.default) { module.exports = exports.default; for (var key in exports) module.exports[key] = exports[key]; }';
+const interopClientDefaultExport = "/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }";
 module.exports = function compileFile(entry, options, callback) {
     fs.readFile(entry.fullPath, 'utf8', (err, contents)=>{
         if (err) return callback(err);
