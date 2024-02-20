b8d3e35bfd90b1772d1d08646ffa3a00949efc1e
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-19 20:44:45 -0800

    Use ts-swc to run mocha

---
+++ src/lib/optionsToArgs.js
@@ -0,0 +1,10 @@
+module.exports = function optionsToArgs(options) {
+  let args = [];
+  for (const key in options) {
+    if (key === '_') continue;
+    if (options[key] === true) args.push(`--${key}`);
+    else if (options[key] === false) args.push(`--no-${key}`);
+    else args = args.concat([`--${key}`, options[key]]);
+  }
+  return args;
+};
