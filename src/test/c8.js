6dd8821c2d4719c649a7c02323e1a09a59033c14
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-19 20:44:45 -0800

    Use ts-swc to run mocha

--- src/test/c8.js
+++ src/test/c8.js
@@ -1,15 +1,19 @@
 const path = require('path');
 const rimraf = require('rimraf');
 const Queue = require('queue-cb');
-const { spawnParams } = require('ts-swc-loaders');
 
 const link = require('../link');
 const spawn = require('../lib/spawn');
+const optionsToArgs = require('../lib/optionsToArgs');
 
 const major = +process.versions.node.split('.')[0];
-const type = major < 12 ? 'commonjs' : 'module';
+const mochaName = major < 12 ? 'mocha-compat' : 'mocha';
 
+let binC8 = null;
+let binMocha = null;
 module.exports = function c8(_args, options, cb) {
+  if (!binC8) binC8 = require.resolve('c8/bin/c8');
+  if (!binMocha) binMocha = require.resolve(`${mochaName}/bin/_${mochaName}`);
   const cwd = options.cwd || process.cwd();
 
   link(_args, options, (err, restore) => {
@@ -22,15 +26,11 @@ module.exports = function c8(_args, options, cb) {
       });
     });
     queue.defer((cb) => {
-      const cmd = require.resolve('c8/bin/c8');
-      let args = ['--config', path.join(__dirname, '..', '..', '..', 'assets', 'c8rc.json'), 'mocha', '--watch-extensions', 'ts,tsx'];
+      let args = [binC8, '--config', path.join(__dirname, '..', '..', '..', 'assets', 'c8rc.json')];
+      args = args.concat([binMocha, '--watch-extensions', 'ts,tsx']);
+      args = args.concat(optionsToArgs(options));
       args = args.concat(_args.length ? _args.slice(-1) : ['test/**/*.test.*']);
-      const params = spawnParams(type, { cwd });
-      if (params.options.NODE_OPTIONS || params.args[0] === '--require') {
-        spawn(cmd, params.args.concat(args), params.options, cb);
-      } else {
-        spawn('node', params.args.concat([cmd]).concat(args), params.options, cb);
-      }
+      spawn('ts-swc', args, { cwd }, cb);
     });
     queue.await((err) => {
       restore((err2) => {
