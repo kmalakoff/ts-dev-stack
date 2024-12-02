c0d379515338c9107709b8e543a16bac966c6871
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-01 17:03:32 -0800

    Upgrade to ts-swc-transform

--- src/build/cjs.js
+++ src/build/cjs.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const Queue = require('queue-cb');
 const rimraf2 = require('rimraf2');
-const compileDirectory = require('./compileDirectory');
+const { transformDirectory } = require('ts-swc-transform');
+
+const source = require('../lib/source');
 
 module.exports = function cjs(_args, options, cb) {
   const cwd = options.cwd || process.cwd();
@@ -11,8 +13,11 @@ module.exports = function cjs(_args, options, cb) {
   options.sourceMaps = true;
   options.dest = path.join(cwd, 'dist', 'cjs');
   rimraf2(options.dest, { disableGlob: true }, () => {
+    const src = source(options);
+    const srcDir = path.dirname(path.resolve(cwd, src));
+
     const queue = new Queue(1);
-    queue.defer(compileDirectory.bind(null, options));
+    queue.defer(transformDirectory.bind(null, srcDir, options.dest, 'cjs', options));
     queue.defer(fs.writeFile.bind(null, path.join(options.dest, 'package.json'), '{"type":"commonjs"}'));
     queue.await(cb);
   });
