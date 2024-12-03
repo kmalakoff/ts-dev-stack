c35d3866ce5e89578ce62cda1c9042c227e6f65b
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-02 23:33:26 -0800

    Moved to lerna

---
+++ packages/ts-dev-stack/src/build/index.js
@@ -0,0 +1,17 @@
+const Queue = require('queue-cb');
+const cjs = require('./cjs');
+const esm = require('./esm');
+const umd = require('./umd');
+const types = require('./types');
+const { targets } = require('tsds-lib');
+
+module.exports = function build(args, options, cb) {
+  const targs = targets(options);
+
+  const queue = new Queue(1);
+  targs.indexOf('cjs') < 0 || queue.defer(cjs.bind(null, args, options));
+  targs.indexOf('esm') < 0 || queue.defer(esm.bind(null, args, options));
+  targs.indexOf('umd') < 0 || queue.defer(umd.bind(null, args, options));
+  queue.defer(types.bind(null, args, options));
+  queue.await(cb);
+};
---
+++ packages/ts-dev-stack/src/unlink.js
@@ -0,0 +1,9 @@
+const { unlink, installPath } = require('tsds-lib');
+
+module.exports = function unlinkCmd(_args, options, cb) {
+  try {
+    unlink(installPath(options), cb);
+  } catch (err) {
+    return cb(err);
+  }
+};
