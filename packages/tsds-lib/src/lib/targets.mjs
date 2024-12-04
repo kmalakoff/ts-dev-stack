aa6baf78c69ee0684938d2a17d33080b2e00a710
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-04 00:34:52 -0800

    Refactor for monorepo

--- packages/tsds-lib/src/lib/targets.mjs
+++ packages/tsds-lib/src/lib/targets.mjs
@@ -1,10 +1,6 @@
-import path from 'path';
-import fs from 'fs';
+import config from './config';
 
 export default function targets(options) {
-  const cwd = options.cwd || process.cwd();
-  const pkg = JSON.parse(fs.readFileSync(path.resolve(cwd, 'package.json'), 'utf8'));
-
-  if (pkg.tsds && pkg.tsds.targets) return pkg.tsds.targets;
-  return ['cjs', 'esm', 'umd'];
+  const tsds = config(options);
+  return tsds.targets || ['cjs', 'esm', 'umd'];
 }
