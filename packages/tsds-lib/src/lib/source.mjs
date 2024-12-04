4aee1e8dad212b57482d3fc8f80cddc069cee762
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-04 00:34:52 -0800

    Refactor for monorepo

--- packages/tsds-lib/src/lib/source.mjs
+++ packages/tsds-lib/src/lib/source.mjs
@@ -1,10 +1,8 @@
-import fs from 'fs';
 import path from 'path';
+import config from './config';
 
 export default function source(options) {
-  const cwd = options.cwd || process.cwd();
-  const pkg = JSON.parse(fs.readFileSync(path.resolve(cwd, 'package.json'), 'utf8'));
-  if (!pkg.tsds || !pkg.tsds.source) console.log('Using default source: src/index.ts. Add "tsds": { "source": "src/index.ts" } to your package.json');
-
-  return (pkg.tsds && pkg.tsds.source ? pkg.tsds.source.split('/') : ['src', 'index.ts']).join(path.sep);
+  const tsds = config(options);
+  if (!tsds.source) console.log('Using default source: src/index.ts. Add "tsds": { "source": "src/index.ts" } to your package.json');
+  return (tsds.source ? tsds.source.split('/') : ['src', 'index.ts']).join(path.sep);
 }
