d0d267138baaed1de824852cbb783c93622f48fb
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-07 13:42:43 -0800

    Release tsds-web-test-runner

--- packages/tsds-build/src/build/umd.mjs
+++ packages/tsds-build/src/build/umd.mjs
@@ -1,9 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 import Queue from 'queue-cb';
-
+import requireResolve from 'resolve/sync';
 import rimraf2 from 'rimraf2';
-import { source, spawn } from 'tsds-lib';
+import { binPath, source, spawn } from 'tsds-lib';
+
+const major = typeof process === 'undefined' ? Infinity : +process.versions.node.split('.')[0];
+const nvu = binPath(requireResolve('node-version-use/package.json', { basedir: __dirname }), 'nvu');
 
 function packageRoot(dir, packageName) {
   if (path.basename(dir) === packageName) return dir;
@@ -23,7 +26,11 @@ export default function umd(_args, options, cb) {
   options.dest = path.join(cwd, 'dist', 'umd');
   rimraf2(options.dest, { disableGlob: true }, () => {
     const queue = new Queue(1);
-    queue.defer(spawn.bind(null, 'rollup', ['--config', config, '--input', src], { cwd }));
+    (() => {
+      let args = ['rollup', '--config', config, '--input', src];
+      if (major < 14) args = [nvu, 'stable'].concat(args);
+      queue.defer(spawn.bind(null, args[0], args.slice(1), { cwd }));
+    })();
     queue.defer(fs.writeFile.bind(null, path.join(options.dest, 'package.json'), '{"type":"commonjs"}'));
     queue.await(cb);
   });
