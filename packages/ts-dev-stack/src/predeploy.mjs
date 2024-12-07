5c8b0948bc4351f15062d874dddf1ac986f0524a
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-07 13:42:43 -0800

    Release tsds-web-test-runner

--- packages/ts-dev-stack/src/predeploy.mjs
+++ packages/ts-dev-stack/src/predeploy.mjs
@@ -1,8 +1,12 @@
 import Queue from 'queue-cb';
-import { spawn } from 'tsds-lib';
-
 import format from 'tsds-biome';
 import build from 'tsds-build';
+
+import requireResolve from 'resolve/sync';
+import { binPath, spawn } from 'tsds-lib';
+
+const major = typeof process === 'undefined' ? Infinity : +process.versions.node.split('.')[0];
+const nvu = binPath(requireResolve('node-version-use/package.json', { basedir: __dirname }), 'nvu');
 
 export default function predeploy(args, options, cb) {
   const cwd = options.cwd || process.cwd();
@@ -10,7 +14,17 @@ export default function predeploy(args, options, cb) {
   const queue = new Queue(1);
   queue.defer(format.bind(null, args, options));
   queue.defer(build.bind(null, args, options));
-  queue.defer(spawn.bind(null, 'sort-package-json', [], { cwd }));
-  queue.defer(spawn.bind(null, 'depcheck', [], { cwd }));
+
+  (() => {
+    let args = ['sort-package-json'];
+    if (major < 14) args = [nvu, 'stable'].concat(args);
+    queue.defer(spawn.bind(null, args[0], args.slice(1), { cwd }));
+  })();
+  (() => {
+    let args = ['depcheck'];
+    if (major < 14) args = [nvu, 'stable'].concat(args);
+    queue.defer(spawn.bind(null, args[0], args.slice(1), { cwd }));
+  })();
+
   queue.await(cb);
 }
