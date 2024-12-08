4c6722b4a7be67a86cae326ce96f01faaac8a97d
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-07 16:00:34 -0800

    Refactor package resolution and update tsds-c8 command logic

--- packages/tsds-lib/src/lib/optionsToArgs.mjs
+++ packages/tsds-lib/src/lib/optionsToArgs.mjs
@@ -1,10 +1,10 @@
 export default function optionsToArgs(options) {
-  let args = [];
+  const args = [];
   for (const key in options) {
     if (key === '_') continue;
     if (options[key] === true) args.push(`--${key}`);
     else if (options[key] === false) args.push(`--no-${key}`);
-    else args = args.concat([`--${key}`, options[key]]);
+    else Array.prototype.push.apply(args, [`--${key}`, options[key]]);
   }
   return args;
 }
