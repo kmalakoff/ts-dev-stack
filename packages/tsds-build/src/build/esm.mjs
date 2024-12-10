2f5a3942855ceacf81a944486937fc20aec2dfb3
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-09 21:29:48 -0800

    Refactored for ts-swc-rollup-plugin

--- packages/tsds-build/src/build/esm.mjs
+++ packages/tsds-build/src/build/esm.mjs
@@ -7,17 +7,14 @@ import { source } from 'tsds-lib';
 
 export default function esm(_args, options, cb) {
   const cwd = options.cwd || process.cwd();
-  options = { ...options };
-  options.type = 'esm';
-  options.sourceMaps = true;
-  options.dest = path.join(cwd, 'dist', 'esm');
-  rimraf2(options.dest, { disableGlob: true }, () => {
-    const src = source(options);
-    const srcDir = path.dirname(path.resolve(cwd, src));
+  const src = source(options);
+  const srcDir = path.dirname(path.resolve(cwd, src));
+  const dest = path.join(cwd, 'dist', 'esm');
 
+  rimraf2(dest, { disableGlob: true }, () => {
     const queue = new Queue(1);
-    queue.defer(transformDirectory.bind(null, srcDir, options.dest, 'esm', options));
-    queue.defer(fs.writeFile.bind(null, path.join(options.dest, 'package.json'), '{"type":"module"}'));
+    queue.defer(transformDirectory.bind(null, srcDir, dest, 'esm', { ...options, type: 'esm', sourceMaps: true }));
+    queue.defer(fs.writeFile.bind(null, path.join(dest, 'package.json'), '{"type":"module"}'));
     queue.await(cb);
   });
 }
