f3704409d90167def9a186fe84e274d3cedeaa1c
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-21 11:43:08 -0800

    Fixed package root

--- packages/tsds-lib/src/lib/packageRoot.ts
+++ packages/tsds-lib/src/lib/packageRoot.ts
@@ -1,8 +1,10 @@
+import fs from 'fs';
 import path from 'path';
 import existsSync from './existsSync.js';
 
 export default function packageRoot(dir) {
-  if (existsSync(path.join(dir, 'package.json'))) return dir;
+  const packagePath = path.join(dir, 'package.json');
+  if (existsSync(packagePath) && JSON.parse(fs.readFileSync(packagePath, 'utf8')).name) return dir;
   const nextDir = path.dirname(dir);
   if (nextDir === dir) throw new Error('Package root not found');
   return packageRoot(nextDir);
