84c81c19fb41d21bbac8ff476ec5130d219e8300
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-21 04:16:29 -0800

    Removing

--- archive/tsds-karma/src/command.ts
+++ archive/tsds-karma/src/command.ts
@@ -1,17 +1,11 @@
 import path from 'path';
 import url from 'url';
 import Queue from 'queue-cb';
-import { installPath, link, spawn } from 'tsds-lib';
+import { installPath, packageRoot, link, spawn } from 'tsds-lib';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 
-function packageRoot(dir, packageName) {
-  if (path.basename(dir) === packageName) return dir;
-  const nextDir = path.dirname(dir);
-  if (nextDir === dir) throw new Error(`${packageName} not found`);
-  return packageRoot(nextDir, packageName);
-}
-const config = path.join(packageRoot(__dirname, 'tsds-karma'), 'assets', 'karma.conf.cjs');
+const config = path.join(packageRoot(__dirname), 'assets', 'karma.conf.cjs');
 
 export default function karma(args, options, cb) {
   link(installPath(options), (_err, restore) => {
