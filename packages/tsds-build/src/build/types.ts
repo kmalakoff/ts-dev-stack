9fd5984fc1663179c61a95037a5c9da300eaab30
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-22 23:47:23 -0800

    Upgrade modules

--- packages/tsds-build/src/build/types.ts
+++ packages/tsds-build/src/build/types.ts
@@ -4,11 +4,11 @@ import Iterator from 'fs-iterator';
 import getTS from 'get-tsconfig-compat';
 import resolve from 'resolve';
 import rimraf2 from 'rimraf2';
-import { createMatcher } from 'ts-swc-transform';
+import { constants, createMatcher } from 'ts-swc-transform';
 import { binPath, source, spawn } from 'tsds-lib';
 
-// @ts-ignore
-import process from '../lib/process.cjs';
+const { typeFileRegEx, SKIPS } = constants;
+
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 
 const major = typeof process === 'undefined' ? Infinity : +process.versions.node.split('.')[0];
@@ -30,19 +30,20 @@ export default function types(_args, options, cb) {
   }
 
   rimraf2(dest, { disableGlob: true }, () => {
-    const files = [];
+    const entries = [];
     const iterator = new Iterator(srcDir);
     iterator.forEach(
-      (entry, callback) => {
-        if (!entry.stats.isFile()) return callback();
-        if (!matcher(entry.fullPath)) return callback();
-        files.push(entry.fullPath);
-        callback();
+      (entry) => {
+        if (!entry.stats.isFile()) return;
+        if (!matcher(entry.fullPath)) return;
+        if (typeFileRegEx.test(entry.basename)) return;
+        if (SKIPS.indexOf(entry.basename) >= 0) return;
+        entries.push(entry);
       },
-      { callbacks: true, concurrency: 1024 },
       (err) => {
         if (err) return cb(err);
 
+        const files = entries.map((entry) => entry.fullPath);
         let args = ['tsc', ...files, '--declaration', '--emitDeclarationOnly', '--outDir', dest, ...tsArgs];
         if (major < 14) args = [nvu, 'stable', ...args];
         spawn(args[0], args.slice(1), { cwd }, cb);
