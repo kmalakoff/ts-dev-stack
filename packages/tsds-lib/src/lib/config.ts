6e85e3c3b21dca4bed288c0efe232634c9e10930
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-23 14:43:34 -0800

    Dynamic lookups of functions

--- packages/tsds-lib/src/lib/config.ts
+++ packages/tsds-lib/src/lib/config.ts
@@ -6,7 +6,28 @@ export interface ConfigOptions {
   config?: JSON;
 }
 
+const defaults = {
+  source: 'src/index.ts',
+  targets: ['cjs', 'esm'],
+  commands: {
+    build: 'tsds-build',
+    coverage: 'tsds-c8',
+    publish: './commands/publish.js',
+    prepublish: './commands/prepublish.js',
+    docs: 'tsds-typedoc',
+    format: 'tsds-biome',
+    link: './commands/link.js',
+    'test:node': 'tsds-mocha',
+    'test:browser': 'tsds-web-test-runner',
+    unlink: './commands/unlink.js',
+    version: './commands/version.js',
+  },
+};
+
 export default function config(options: ConfigOptions = {}) {
+  if (options.config) return options.config;
   const cwd = options.cwd || process.cwd();
-  return options.config || JSON.parse(fs.readFileSync(path.resolve(cwd, 'package.json'), 'utf8')).tsds || {};
+  const tsds = JSON.parse(fs.readFileSync(path.resolve(cwd, 'package.json'), 'utf8')).tsds || {};
+  if (!tsds.source) console.log('Using default source: src/index.ts. Add "tsds": { "source": "src/index.ts" } to your package.json');
+  return { ...defaults, ...tsds };
 }
--- packages/tsds-lib/src/lib/targets.ts
+++
@@ -1,6 +0,0 @@
-import config from './config.js';
-
-export default function targets(options) {
-  const tsds = config(options);
-  return tsds.targets || ['cjs', 'esm', 'umd'];
-}
