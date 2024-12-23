4fd791b4656569cf481124adf942d045e2d0fbdb
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-23 15:37:53 -0800

    Upgrade modules

--- packages/ts-dev-stack/src/runCommand.ts
+++ packages/ts-dev-stack/src/runCommand.ts
@@ -1,10 +1,22 @@
 import path from 'path';
 import url from 'url';
+import installModule from 'install-module-linked';
+import resolve from 'resolve';
 import { constants, config } from 'tsds-lib';
 
 // @ts-ignore
 import lazy from './lazy.cjs';
 const ext = path.extname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
+
+function run(specifier, args, options, cb) {
+  try {
+    const mod = lazy(specifier)();
+    const fn = mod.default || mod;
+    fn(args, options, cb);
+  } catch (err) {
+    return cb(err);
+  }
+}
 
 export default function runCommand(name, args, options, cb) {
   const { commands } = config();
@@ -12,13 +24,18 @@ export default function runCommand(name, args, options, cb) {
   const command = commands[name];
   if (!command) return cb(new Error(`Unrecognized command: ${name} ${args.join(' ')}`));
 
-  try {
-    // for relative files, ensure the extension matches
-    const specifier = constants.moduleRegEx.test(command) ? command : command.replace(/\.[^/.]+$/, ext);
-    const mod = lazy(specifier)();
-    const fn = mod.default || mod;
-    fn(args, options, cb);
-  } catch (err) {
-    return cb(err);
+  if (constants.moduleRegEx.test(command)) {
+    try {
+      resolve.sync(path.join(command, 'package.json'));
+      return run(command, args, options, cb);
+    } catch (_err) {
+      const nodeModules = path.dirname(path.dirname(resolve.sync(path.join('tsds-lib', 'package.json'))));
+      return installModule(command, nodeModules, (err) => {
+        console.log(`Module missing: ${command}. ${err ? `Failed install: ${err.message}` : 'Installed'}`);
+        err ? cb(err) : run(command, args, options, cb);
+      });
+    }
   }
+  // for relative files, ensure the extension matches
+  return run(command.replace(/\.[^/.]+$/, ext), args, options, cb);
 }
