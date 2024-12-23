33faf2f5a2659b2af1dab6449f5297a18d7db3cb
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-23 14:43:34 -0800

    Dynamic lookups of functions

--- packages/tsds-build/src/build/types.ts
+++ packages/tsds-build/src/build/types.ts
@@ -1,15 +1,14 @@
 import path from 'path';
 import rimraf2 from 'rimraf2';
 import { transformTypes } from 'ts-swc-transform';
-import { source } from 'tsds-lib';
+import { config } from 'tsds-lib';
 
 export default function cjs(_args, options, cb) {
   const cwd = options.cwd || process.cwd();
-  const src = source(options);
-  const srcDir = path.dirname(path.resolve(cwd, src));
+  const src = path.dirname(path.resolve(cwd, config(options).source));
   const dest = path.join(cwd, 'dist', 'types');
 
   rimraf2(dest, { disableGlob: true }, () => {
-    transformTypes(srcDir, dest, cb);
+    transformTypes(src, dest, cb);
   });
 }
--- packages/tsds-lib/src/index.ts
+++ packages/tsds-lib/src/index.ts
@@ -1,12 +1,11 @@
 export { default as binPath } from './lib/binPath.js';
 export { default as config } from './lib/config.js';
+export * as constants from './lib/constants.js';
 export { default as installPath } from './lib/installPath.js';
 export { default as link } from './lib/link.js';
 export { default as optionsToArgs } from './lib/optionsToArgs.js';
 export { default as packageRoot } from './lib/packageRoot.js';
 export { default as packageVersion } from './lib/packageVersion.js';
-export { default as source } from './lib/source.js';
 export { default as spawn } from './lib/spawn.js';
-export { default as targets } from './lib/targets.js';
 export { default as unlink } from './lib/unlink.js';
 export { default as uuid } from './lib/uuid.js';
