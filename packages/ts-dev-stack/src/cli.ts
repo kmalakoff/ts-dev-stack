162cb66257574856e3f61b90a6d8c956e124437b
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-23 15:37:53 -0800

    Implement automatic module installation in runCommand

--- packages/ts-dev-stack/src/cli.ts
+++ packages/ts-dev-stack/src/cli.ts
@@ -1,5 +1,4 @@
 import exit from 'exit';
-import getopts from 'getopts-compat';
 import runCommand from './runCommand.js';
 
 export default function cli(argv, name) {
@@ -8,9 +7,7 @@ export default function cli(argv, name) {
     return exit(-1);
   }
 
-  const options = getopts(argv.slice(1), { stopEarly: true });
-  const args = argv.slice(0, 1).concat(options._);
-  runCommand(argv[0], args.slice(1), options, (err) => {
+  runCommand(argv[0], argv.slice(1), {}, (err) => {
     if (err && err.message.indexOf('ExperimentalWarning') < 0) {
       console.log(err.message);
       return exit(err.code || -1);
