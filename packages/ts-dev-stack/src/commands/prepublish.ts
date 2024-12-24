5fa85664d961175bdc2a9f01fac3ce861067ecde
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 09:34:28 -0800

    Upgrade modules

--- packages/ts-dev-stack/src/commands/prepublish.ts
+++ packages/ts-dev-stack/src/commands/prepublish.ts
@@ -3,32 +3,23 @@ import url from 'url';
 import Queue from 'queue-cb';
 import format from 'tsds-biome';
 import build from 'tsds-build';
-
-import resolve from 'resolve';
-import { binPath, spawn } from 'tsds-lib';
+import { packageRoot, spawn, wrapWorker } from 'tsds-lib';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
-
 const major = +process.versions.node.split('.')[0];
-const nvu = binPath(resolve.sync('node-version-use/package.json', { basedir: __dirname }), 'nvu');
+const workerWrapper = wrapWorker(path.join(packageRoot(__dirname), 'dist', 'cjs', 'commands', 'prepublish.js'));
 
-export default function prepublish(args, options, cb) {
+function worker(args, options, cb) {
   const cwd = options.cwd || process.cwd();
 
   const queue = new Queue(1);
   queue.defer(format.bind(null, args, options));
   queue.defer(build.bind(null, args, options));
-
-  (() => {
-    let args = ['sort-package-json'];
-    if (major < 14) args = [nvu, 'stable', ...args];
-    queue.defer(spawn.bind(null, args[0], args.slice(1), { cwd }));
-  })();
-  (() => {
-    let args = ['depcheck'];
-    if (major < 14) args = [nvu, 'stable', ...args];
-    queue.defer(spawn.bind(null, args[0], args.slice(1), { cwd }));
-  })();
-
+  queue.defer(spawn.bind(null, 'sort-package-json', [], { cwd }));
+  queue.defer(spawn.bind(null, 'depcheck', [], { cwd }));
   queue.await(cb);
+}
+
+export default function prepublish(args, options, cb) {
+  major < 14 ? workerWrapper('stable', args, options, cb) : worker(args, options, cb);
 }
