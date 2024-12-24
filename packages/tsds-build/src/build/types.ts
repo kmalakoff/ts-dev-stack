74c64cc21cff8285f03b0eac7a396d1cac0298b5
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-24 09:34:28 -0800

    Refactor CLI tools to use worker wrappers and lazy loading

--- packages/tsds-build/src/build/types.ts
+++ packages/tsds-build/src/build/types.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import Queue from 'queue-cb';
 import rimraf2 from 'rimraf2';
 import { transformTypes } from 'ts-swc-transform';
 import { config } from 'tsds-lib';
@@ -8,7 +9,8 @@ export default function cjs(_args, options, cb) {
   const src = path.dirname(path.resolve(cwd, config(options).source));
   const dest = path.join(cwd, 'dist', 'types');
 
-  rimraf2(dest, { disableGlob: true }, () => {
-    transformTypes(src, dest, cb);
-  });
+  const queue = new Queue(1);
+  queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
+  queue.defer(transformTypes.bind(null, src, dest));
+  queue.await(cb);
 }
