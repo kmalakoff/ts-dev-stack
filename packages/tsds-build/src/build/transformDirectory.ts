a645ff9e11b1582ed515eb580ba67063b4c7b93b
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-25 12:21:31 -0800

    Upgrade modules

--- packages/tsds-build/src/build/transformDirectory.ts
+++ packages/tsds-build/src/build/transformDirectory.ts
@@ -5,14 +5,19 @@ import rimraf2 from 'rimraf2';
 import { transformDirectory } from 'ts-swc-transform';
 import { config } from 'tsds-lib';
 
-export default function cjs(_args, options, cb) {
+export default function transform(_args, type, options, cb) {
   const cwd = options.cwd || process.cwd();
   const src = path.dirname(path.resolve(cwd, config(options).source));
-  const dest = path.join(cwd, 'dist', 'cjs');
+  const dest = path.join(cwd, 'dist', type);
 
   const queue = new Queue(1);
   queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
-  queue.defer(transformDirectory.bind(null, src, dest, 'cjs', { ...options, type: 'cjs', sourceMaps: true }));
+  queue.defer((cb) =>
+    transformDirectory(src, dest, type, { ...options, type, sourceMaps: true }, (err, results) => {
+      if (results && results.to) console.log(err ? `${type} failed: ${err.message} from ${src}` : `created ${results.to.join(',')}`);
+      cb(err);
+    })
+  );
   queue.defer(fs.writeFile.bind(null, path.join(dest, 'package.json'), '{"type":"commonjs"}'));
   queue.await(cb);
 }
--- packages/tsds-build/src/command.ts
+++ packages/tsds-build/src/command.ts
@@ -2,8 +2,7 @@ import path from 'path';
 import Queue from 'queue-cb';
 import rimraf2 from 'rimraf2';
 import { config } from 'tsds-lib';
-import cjs from './build/cjs.js';
-import esm from './build/esm.js';
+import transformDirectory from './build/transformDirectory.js';
 import types from './build/types.js';
 import umd from './build/umd.js';
 
@@ -14,8 +13,8 @@ export default function build(args, options, cb) {
   const dest = path.join(cwd, 'dist');
   const queue = new Queue(1);
   !clean || queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
-  targets.indexOf('cjs') < 0 || queue.defer(cjs.bind(null, args, options));
-  targets.indexOf('esm') < 0 || queue.defer(esm.bind(null, args, options));
+  targets.indexOf('cjs') < 0 || queue.defer(transformDirectory.bind(null, args, 'cjs', options));
+  targets.indexOf('esm') < 0 || queue.defer(transformDirectory.bind(null, args, 'esm', options));
   targets.indexOf('umd') < 0 || queue.defer(umd.bind(null, args, options));
   queue.defer(types.bind(null, args, options));
   queue.await(cb);
