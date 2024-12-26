18eac5350475f5ce36d861b65024fba3d0ffffde
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-26 14:19:05 -0800

    Migrate build system from Rollup to Rolldown

--- packages/tsds-build/src/lib/umd.ts
+++ packages/tsds-build/src/lib/umd.ts
@@ -3,22 +3,28 @@ import path from 'path';
 import url from 'url';
 import Queue from 'queue-cb';
 import rimraf2 from 'rimraf2';
-import { config, packageRoot, spawn, wrapWorker } from 'tsds-lib';
+import { packageRoot, spawn, wrapWorker } from 'tsds-lib';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 const major = +process.versions.node.split('.')[0];
 const workerWrapper = wrapWorker(path.join(packageRoot(__dirname), 'dist', 'cjs', 'build', 'umd.js'));
-const configPath = path.join(packageRoot(__dirname), 'dist', 'esm', 'rollup.config.mjs');
 
 function worker(_args, options, cb) {
   const cwd = options.cwd || process.cwd();
-  const src = path.resolve(cwd, config(options).source);
   const dest = path.join(cwd, 'dist', 'umd');
-  const args = ['rollup', '--config', configPath, '--input', src];
 
   const queue = new Queue(1);
   queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
-  queue.defer(spawn.bind(null, args[0], args.slice(1), { cwd }));
+  (() => {
+    const configPath = path.join(packageRoot(__dirname), 'dist', 'esm', 'rollup', 'config.mjs');
+    const args = ['rollup', '--config', configPath];
+    queue.defer(spawn.bind(null, args[0], args.slice(1), { cwd }));
+  })();
+  (() => {
+    const configPath = path.join(packageRoot(__dirname), 'dist', 'esm', 'rollup', 'config.min.mjs');
+    const args = ['rollup', '--config', configPath];
+    queue.defer(spawn.bind(null, args[0], args.slice(1), { cwd }));
+  })();
   queue.defer(fs.writeFile.bind(null, path.join(dest, 'package.json'), '{"type":"commonjs"}'));
   queue.await(cb);
 }
