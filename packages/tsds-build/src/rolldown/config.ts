2adf9a8f2286bafbeb492c2c95315365b13eed2f
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-26 14:19:05 -0800

    Migrate build system from Rollup to Rolldown

---
+++ packages/tsds-build/src/rolldown/config.ts
@@ -0,0 +1,27 @@
+import path from 'path';
+import camelcase from 'lodash.camelcase';
+import { type Plugin, defineConfig } from 'rolldown';
+import swc from 'ts-swc-rollup-plugin';
+
+import { globals, input, pkg, tsconfig } from '../lib/rollingOptions.js';
+
+export const moduleRegEx = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/;
+
+import fs from 'fs';
+fs.writeFileSync(`${tsconfig.path}.es5`, JSON.stringify(tsconfig.config), 'utf8');
+
+export default defineConfig({
+  input,
+  output: {
+    file: path.resolve(process.cwd(), 'dist', 'umd', `${pkg.name}.js`),
+    format: 'umd',
+    sourcemap: true,
+    name: camelcase(pkg.name),
+    globals,
+  },
+  external: (module) => !!moduleRegEx.test(module),
+  plugins: [swc({ tsconfig }) as unknown as Plugin],
+  resolve: {
+    tsconfigFilename: `${tsconfig.path}.es5`,
+  },
+});
