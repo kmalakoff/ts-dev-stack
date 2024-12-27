ffddb38da5aa4d921325188a58bf3e95d3587ec3
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-27 11:41:26 -0800

    Upgrade modules

--- packages/tsds-karma/src/command.ts
+++ packages/tsds-karma/src/command.ts
@@ -1,27 +1,30 @@
 import path from 'path';
 import url from 'url';
 import { link, unlink } from 'link-unlink';
-import Queue from 'queue-cb';
-import { installPath, packageRoot, spawn } from 'tsds-lib';
+import Queue from 'queue-callback';
+import { installPath, packageRoot, spawn, which } from 'tsds-lib';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 
 const config = path.join(packageRoot(__dirname), 'assets', 'karma.conf.cjs');
 
-export default function karma(args, options, cb) {
+export default function karma(args, options, callback) {
   const cwd = options.cwd || process.cwd();
 
-  link(cwd, installPath(options), (err, restore) => {
-    if (err) return cb(err);
+  which('karma', options, (err, karma) => {
+    if (err) return callback(err);
+    link(cwd, installPath(options), (err, restore) => {
+      if (err) return callback(err);
 
-    const queue = new Queue(1);
-    queue.defer((cb) => {
-      const tests = args.length ? args[0] : 'test/**/*.test.*';
-      spawn('karma', ['start', config, tests], {}, cb);
-    });
-    queue.await((err) => {
-      unlink(restore, (err2) => {
-        cb(err || err2);
+      const queue = new Queue(1);
+      queue.defer((callback) => {
+        const tests = args.length ? args[0] : 'test/**/*.test.*';
+        spawn(karma, ['start', config, tests], {}, callback);
+      });
+      queue.await((err) => {
+        unlink(restore, (err2) => {
+          callback(err || err2);
+        });
       });
     });
   });
