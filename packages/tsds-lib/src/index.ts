58500623a1f73ee51d1d3dfea391cd5695f945d0
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-27 08:18:44 -0800

    Upgrade modules

--- packages/tsds-lib/src/index.ts
+++ packages/tsds-lib/src/index.ts
@@ -1,9 +1,9 @@
-export { default as binPath } from './lib/binPath.js';
 export { default as config } from './lib/config.js';
 export * as constants from './lib/constants.js';
 export { default as installPath } from './lib/installPath.js';
 export { default as packageRoot } from './lib/packageRoot.js';
 export { default as packageVersion } from './lib/packageVersion.js';
 export { default as spawn } from './lib/spawn.js';
-export { default as uuid } from './lib/uuid.js';
+export { default as which } from './lib/which.js';
+export { default as whichAll } from './lib/whichAll.js';
 export { default as wrapWorker } from './lib/wrapWorker.js';
