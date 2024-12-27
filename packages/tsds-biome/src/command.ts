c0f6404429b58aaf02888fde2609390e9809f6f4
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-27 11:41:26 -0800

    Use which to resolve command paths before spawning

--- packages/tsds-biome/src/command.ts
+++ packages/tsds-biome/src/command.ts
@@ -1,14 +1,16 @@
 import path from 'path';
 import url from 'url';
-import { packageRoot, spawn, wrapWorker } from 'tsds-lib';
+import { packageRoot, spawn, which, wrapWorker } from 'tsds-lib';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 const major = +process.versions.node.split('.')[0];
 const workerWrapper = wrapWorker(path.join(packageRoot(__dirname), 'dist', 'cjs', 'command.js'));
 
-function worker(_args, options, cb) {
+function worker(_args, options, callback) {
   const cwd = options.cwd || process.cwd();
-  spawn('npm', ['run', 'format'], { cwd }, cb);
+  which('npm', options, (err, npm) => {
+    err ? callback(err) : spawn(npm, ['run', 'format'], { cwd }, callback);
+  });
 }
 
 export default function format(args, options, cb) {
