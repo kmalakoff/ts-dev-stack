f428891feeffe9d15db405395a69670a52d707eb
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 00:36:29 -0800

    Add version flag to CLI

--- packages/ts-dev-stack/src/cli.ts
+++ packages/ts-dev-stack/src/cli.ts
@@ -1,10 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
 import exit from 'exit';
+import getopts from 'getopts-compat';
 import runCommand from './runCommand.js';
+
+const _dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 
 export default function cli(argv, name) {
   if (argv.length === 0) {
     console.log(`Command missing ${name} ${argv.join(',')}`);
     return exit(-1);
+  }
+
+  const { _, ...opts } = getopts(argv, { stopEarly: true, alias: { version: 'v' }, boolean: ['version'] });
+  if (opts.version) {
+    const pkg = JSON.parse(fs.readFileSync(path.join(_dirname, '..', '..', 'package.json'), 'utf8'));
+    console.log(pkg.version);
+    return exit(0);
   }
 
   runCommand(argv[0], argv.slice(1), {}, (err) => {
