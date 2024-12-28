5a75afbd708451e840eead1a8b4a384b0fdc343e
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 13:26:03 -0800

    Replace packageRoot with moduleRoot and update dependencies

--- packages/tsds-karma/src/command.ts
+++ packages/tsds-karma/src/command.ts
@@ -1,12 +1,14 @@
 import path from 'path';
 import url from 'url';
 import { link, unlink } from 'link-unlink';
+import moduleRoot from 'module-root-sync';
+import which from 'module-which';
 import Queue from 'queue-cb';
-import { installPath, packageRoot, spawn, which } from 'tsds-lib';
+import { installPath, spawn } from 'tsds-lib';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
 
-const config = path.join(packageRoot(__dirname), 'assets', 'karma.conf.cjs');
+const config = path.join(moduleRoot(__dirname), 'assets', 'karma.conf.cjs');
 
 export default function karma(args, options, callback) {
   const cwd = options.cwd || process.cwd();
--- packages/tsds-lib/src/lib/spawn.ts
+++ packages/tsds-lib/src/lib/spawn.ts
@@ -1,19 +1,19 @@
 import path from 'path';
 import url from 'url';
 import crossSpawn from 'cross-spawn-cb';
-import pathKey from 'env-path-key';
+import envPathKey from 'env-path-key';
+import moduleRoot from 'module-root-sync';
 import prepend from 'path-string-prepend';
 
-import packageRoot from './packageRoot.js';
-
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
-const root = packageRoot(__dirname);
+const root = moduleRoot(__dirname);
 
 export default function spawn(cmd, args, options, cb) {
   const cwd = options.cwd || process.cwd();
-  const PATH_KEY = pathKey(options);
-  const env = { ...process.env, env: options.env || {} };
-  env[PATH_KEY] = prepend(env[PATH_KEY] || '', path.resolve(root, '..', '..', 'node_modules', '.bin'));
-  env[PATH_KEY] = prepend(env[PATH_KEY] || '', path.resolve(cwd, 'node_modules', '.bin'));
-  crossSpawn(cmd, args, { stdio: 'inherit', cwd, env: env }, cb);
+  const env = options.env || process.env;
+  const pathKey = envPathKey(env) || '';
+  let envPath = env[pathKey] || '';
+  envPath = prepend(envPath, path.resolve(cwd, 'node_modules', '.bin'));
+  envPath = prepend(envPath, path.resolve(root, '..', '.bin'));
+  crossSpawn(cmd, args, { stdio: 'inherit', cwd, env: { ...env, [pathKey]: envPath } }, cb);
 }
