961d78d7f7592f8db023c8a6acbdebae45806c23
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 04:40:51 -0800

    Upgrade modules

--- packages/tsds-build/src/command.ts
+++ packages/tsds-build/src/command.ts
@@ -2,7 +2,8 @@ import path from 'path';
 import Queue from 'queue-cb';
 import rimraf2 from 'rimraf2';
 import { config } from 'tsds-lib';
-import transformDirectory from './lib/transformDirectory.js';
+import docs from 'tsds-typedoc';
+import code from './lib/code.js';
 import types from './lib/types.js';
 import umd from './lib/umd.js';
 
@@ -13,9 +14,10 @@ export default function build(args, options, cb) {
   const dest = path.join(cwd, 'dist');
   const queue = new Queue(1);
   !clean || queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
-  targets.indexOf('cjs') < 0 || queue.defer(transformDirectory.bind(null, args, 'cjs', options));
-  targets.indexOf('esm') < 0 || queue.defer(transformDirectory.bind(null, args, 'esm', options));
+  targets.indexOf('cjs') < 0 || queue.defer(code.bind(null, args, 'cjs', options));
+  targets.indexOf('esm') < 0 || queue.defer(code.bind(null, args, 'esm', options));
   targets.indexOf('umd') < 0 || queue.defer(umd.bind(null, args, options));
   queue.defer(types.bind(null, args, options));
+  queue.defer(docs.bind(null, args, options));
   queue.await(cb);
 }
