be41a13a2e00550a26d8a960f1486be6726b30b2
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 08:56:55 -0800

    Upgrade modules

--- packages/tsds-mocha/scripts/postinstall.cjs
+++ packages/tsds-mocha/scripts/postinstall.cjs
@@ -4,37 +4,38 @@ const path = require('path');
 const fs = require('fs');
 const Queue = require('queue-cb');
 const unixify = require('unixify');
-const _exit = require('exit');
+const { whichAll } = require('tsds-lib');
 
 const FILES = ['lib/cli/lookup-files.js'];
 
 function patch(callback) {
-  let patchPath;
-  let mochaCompatPath;
-  try {
-    patchPath = path.dirname(require.resolve('mocha/package.json'));
-    mochaCompatPath = require.resolve('mocha-compat/vendor/glob/glob.js');
-  } catch (err) {
-    return callback(err);
-  }
+  whichAll(['mocha', 'mocha-compat'], {}, (err, results) => {
+    if (err) return callback(err);
+    const patchPath = path.join(results[0], '..', 'mocha');
+    const mochaCompatPath = path.join(results[0], '..', 'mocha-compat');
 
-  const queue = new Queue();
-  FILES.map((file) => {
-    queue.defer((cb) => {
-      const filePath = path.join.apply(null, [patchPath].concat(file.split('/')));
-      const contents = fs.readFileSync(filePath, 'utf8');
-      const newContents = contents.replace("require('glob')", `require('${unixify(path.relative(path.dirname(filePath), mochaCompatPath))}')`);
-      if (contents === newContents) return cb(); // no change
-      fs.writeFileSync(filePath, newContents, 'utf8');
-      console.log(`Patched glob in: ${filePath}`);
-      cb();
+    const queue = new Queue();
+    FILES.map((file) => {
+      queue.defer((cb) => {
+        try {
+          const filePath = path.join.apply(null, [patchPath].concat(file.split('/')));
+          const contents = fs.readFileSync(filePath, 'utf8');
+          const newContents = contents.replace("require('glob')", `require('${unixify(path.relative(path.dirname(filePath), mochaCompatPath))}')`);
+          if (contents === newContents) return cb(); // no change
+          fs.writeFileSync(filePath, newContents, 'utf8');
+          console.log(`Patched glob in: ${filePath}`);
+          cb();
+        } catch (err) {
+          return callback(err);
+        }
+      });
     });
+    queue.await(callback);
   });
-  queue.await(callback);
 }
 
 // run patch
 patch((err) => {
   !err || console.log(err.message);
-  process.exit(err ? 1 : 0);
+  process.exit(0);
 });
