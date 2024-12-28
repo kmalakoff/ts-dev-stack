a9b166aba3b0a8b41fac997e0722b7c9d83f5dd2
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 10:19:41 -0800

    Update tsds-mocha postinstall script to improve node_modules lookup

--- packages/tsds-mocha/scripts/postinstall.cjs
+++ packages/tsds-mocha/scripts/postinstall.cjs
@@ -8,29 +8,44 @@ const { whichAll } = require('tsds-lib');
 
 const FILES = ['lib/cli/lookup-files.js'];
 
+function findNodeModules(found) {
+  const foundParts = found.split(path.sep);
+  while (foundParts.length) {
+    if (foundParts[foundParts.length - 1] === 'node_modules') return foundParts.join(path.sep);
+    // global install
+    if (foundParts[foundParts.length - 1] === 'bin') return foundParts.slice(0, -1).concat(['lib', 'node_modules']).join(path.sep);
+    foundParts.pop()
+  }
+  throw new Error('node_modules not found');
+}
+
 function patch(callback) {
   whichAll(['mocha', 'mocha-compat'], {}, (err, results) => {
     if (err) return callback(err);
-    const patchPath = path.join(results[0], '..', 'mocha');
-    const mochaCompatPath = path.join(results[0], '..', 'mocha-compat');
+    try {
+      const patchPath = path.join(findNodeModules(results[0]), 'mocha')
+      const mochaCompatPath = path.join(findNodeModules(results[1]), 'mocha-compat', 'vendor', 'glob')
 
-    const queue = new Queue();
-    FILES.map((file) => {
-      queue.defer((cb) => {
-        try {
-          const filePath = path.join.apply(null, [patchPath].concat(file.split('/')));
-          const contents = fs.readFileSync(filePath, 'utf8');
-          const newContents = contents.replace("require('glob')", `require('${unixify(path.relative(path.dirname(filePath), mochaCompatPath))}')`);
-          if (contents === newContents) return cb(); // no change
-          fs.writeFileSync(filePath, newContents, 'utf8');
-          console.log(`Patched glob in: ${filePath}`);
-          cb();
-        } catch (err) {
-          return callback(err);
-        }
+      const queue = new Queue();
+      FILES.map((file) => {
+        queue.defer((cb) => {
+          try {
+            const filePath = path.join.apply(null, [patchPath].concat(file.split('/')));
+            const contents = fs.readFileSync(filePath, 'utf8');
+            const newContents = contents.replace("require('glob')", `require('${unixify(path.relative(path.dirname(filePath), mochaCompatPath))}')`);
+            if (contents === newContents) return cb(); // no change
+            fs.writeFileSync(filePath, newContents, 'utf8');
+            console.log(`Patched glob in: ${filePath}`);
+            cb();
+          } catch (err) {
+            return callback(err);
+          }
+        });
       });
-    });
-    queue.await(callback);
+      queue.await(callback);
+    } catch (err) {
+      return callback(err);
+    }
   });
 }
 
