d6f4b5475f93e0d7ca3186a9ea016c3f52254f72
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-29 14:49:31 -0800

    Upgrade modules

--- packages/tsds-typedoc/src/command.ts
+++ packages/tsds-typedoc/src/command.ts
@@ -3,28 +3,29 @@ import url from 'url';
 import spawn from 'cross-spawn-cb';
 import mkdirp from 'mkdirp-classic';
 import moduleRoot from 'module-root-sync';
-import { prependEnvPath } from 'module-which';
 import Queue from 'queue-cb';
+import resolveBin from 'resolve-bin';
 import rimraf2 from 'rimraf2';
 import { config, wrapWorker } from 'tsds-lib';
 
 const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
-const root = moduleRoot(__dirname);
 const major = +process.versions.node.split('.')[0];
 const workerWrapper = wrapWorker(path.join(moduleRoot(__dirname), 'dist', 'cjs', 'command.js'));
 
 function worker(_args, options, callback) {
-  const cwd = options.cwd || process.cwd();
-  const { envPath, pathKey } = prependEnvPath({ root, ...options });
-  const env = { ...(options.env || process.env), [pathKey]: envPath };
-  const source = config(options).source;
-  const dest = path.resolve(process.cwd(), 'docs');
+  try {
+    const typedoc = resolveBin.sync('typedoc');
+    const source = config(options).source;
+    const dest = path.resolve(process.cwd(), 'docs');
 
-  const queue = new Queue(1);
-  queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
-  queue.defer(mkdirp.bind(null, dest));
-  queue.defer(spawn.bind(null, 'typedoc', [source], { cwd, env }));
-  queue.await(callback);
+    const queue = new Queue(1);
+    queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
+    queue.defer(mkdirp.bind(null, dest));
+    queue.defer(spawn.bind(null, typedoc, [source], options));
+    queue.await(callback);
+  } catch (err) {
+    return callback(err);
+  }
 }
 
 export default function docs(args, options, callback) {
