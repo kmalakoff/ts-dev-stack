207c6f64299b62e20c8155ef4cd0db5e55b44bd1
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-29 14:49:31 -0800

    Replace module-which with resolve-bin for binary resolution

--- packages/tsds-build/src/lib/umd.ts
+++ packages/tsds-build/src/lib/umd.ts
@@ -4,6 +4,7 @@ import url from 'url';
 import spawn from 'cross-spawn-cb';
 import moduleRoot from 'module-root-sync';
 import Queue from 'queue-cb';
+import resolveBin from 'resolve-bin';
 import rimraf2 from 'rimraf2';
 import { wrapWorker } from 'tsds-lib';
 
@@ -11,17 +12,23 @@ const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : 
 const major = +process.versions.node.split('.')[0];
 const workerWrapper = wrapWorker(path.join(moduleRoot(__dirname), 'dist', 'cjs', 'build', 'umd.js'));
 
-function worker(_args, options, cb) {
+function worker(_args, options, callback) {
   const cwd = options.cwd || process.cwd();
   const dest = path.join(cwd, 'dist', 'umd');
   const configRoot = path.join(moduleRoot(__dirname), 'dist', 'esm', 'rollup');
 
-  const queue = new Queue(1);
-  queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
-  queue.defer(spawn.bind(null, 'rollup', ['--config', path.join(configRoot, 'config.mjs')], options));
-  queue.defer(spawn.bind(null, 'rollup', ['--config', path.join(configRoot, 'config.min.mjs')], options));
-  queue.defer(fs.writeFile.bind(null, path.join(dest, 'package.json'), '{"type":"commonjs"}'));
-  queue.await(cb);
+  try {
+    const rollup = resolveBin.sync('rollup');
+
+    const queue = new Queue(1);
+    queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
+    queue.defer(spawn.bind(null, rollup, ['--config', path.join(configRoot, 'config.mjs')], options));
+    queue.defer(spawn.bind(null, rollup, ['--config', path.join(configRoot, 'config.min.mjs')], options));
+    queue.defer(fs.writeFile.bind(null, path.join(dest, 'package.json'), '{"type":"commonjs"}'));
+    queue.await(callback);
+  } catch (err) {
+    return callback(err);
+  }
 }
 
 export default function umd(args, options, cb) {
