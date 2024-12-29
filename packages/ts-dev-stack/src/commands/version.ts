d9078ae4d432b5703273e71681f0226b3c6acf0e
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-29 02:47:22 -0800

    Upgrade modules

--- packages/ts-dev-stack/src/commands/version.ts
+++ packages/ts-dev-stack/src/commands/version.ts
@@ -6,16 +6,9 @@ import { prependEnvPath } from 'module-which';
 import Queue from 'queue-cb';
 import docs from 'tsds-typedoc';
 
-const __dirname = path.dirname(typeof __filename !== 'undefined' ? __filename : url.fileURLToPath(import.meta.url));
-const root = moduleRoot(__dirname);
-
 export default function version(args, options, callback) {
-  const cwd = options.cwd || process.cwd();
-  const { envPath, pathKey } = prependEnvPath({ root, ...options });
-  const env = { ...(options.env || process.env), [pathKey]: envPath };
-
   const queue = new Queue(1);
   queue.defer(docs.bind(null, args, options));
-  queue.defer(spawn.bind(null, 'gh-pages', ['-d', 'docs'], { cwd, env }));
+  queue.defer(spawn.bind(null, 'gh-pages', ['-d', 'docs'], options));
   queue.await(callback);
 }
