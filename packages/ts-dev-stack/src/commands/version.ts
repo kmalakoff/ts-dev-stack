da1d73b9e2357ed0b4951f8502c85c5606a46dee
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-29 00:09:14 -0800

    Replace module-which with resolve in version and typedoc commands

--- packages/ts-dev-stack/src/commands/version.ts
+++ packages/ts-dev-stack/src/commands/version.ts
@@ -1,17 +1,18 @@
 import spawn from 'cross-spawn-cb';
-import which from 'module-which';
 import Queue from 'queue-cb';
+import resolve from 'resolve';
 import docs from 'tsds-typedoc';
 
 export default function version(args, options, callback) {
-  const cwd = options.cwd || process.cwd();
+  try {
+    const cwd = options.cwd || process.cwd();
+    const pages = resolve.sync('gh-pages/bin/gh-pages');
 
-  const queue = new Queue(1);
-  queue.defer(docs.bind(null, args, options));
-  queue.defer((_cb) => {
-    which('gh-pages', options, (err, pages) => {
-      err ? callback(err) : spawn(pages, ['-d', 'docs'], { cwd }, callback);
-    });
-  });
-  queue.await(callback);
+    const queue = new Queue(1);
+    queue.defer(docs.bind(null, args, options));
+    queue.defer(spawn.bind(null, pages, ['-d', 'docs'], { cwd }));
+    queue.await(callback);
+  } catch (err) {
+    return callback(err);
+  }
 }
