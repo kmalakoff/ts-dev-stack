39badc2fb807be6675b3159480edb7122cf78028
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-28 16:04:47 -0800

    Upgrade modules

--- dev/build/assets/tsds-build.js
+++ dev/build/assets/tsds-build.js
@@ -5,7 +5,7 @@ var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __commonJS = (cb, mod) => function () {
+var __commonJS = (cb, mod) => function() {
 	return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
 };
 var __copyProps = (to, from, except, desc) => {
@@ -28,16 +28,15 @@ const path = __toESM(require("path"));
 const queue_cb = __toESM(require("queue-cb"));
 const rimraf2 = __toESM(require("rimraf2"));
 const fs = __toESM(require("fs"));
-const fs_exists_sync = __toESM(require("fs-exists-sync"));
+const module_root_sync = __toESM(require("module-root-sync"));
 const url = __toESM(require("url"));
 const cross_spawn_cb = __toESM(require("cross-spawn-cb"));
 const env_path_key = __toESM(require("env-path-key"));
 const path_string_prepend = __toESM(require("path-string-prepend"));
-require("which");
 const tsds_typedoc = __toESM(require("tsds-typedoc"));
 const ts_swc_transform = __toESM(require("ts-swc-transform"));
 
-//#region node_modules/tsds-lib/dist/esm/lib/config.mjs
+//#region ../../packages/tsds-lib/dist/esm/lib/config.mjs
 const defaults = {
 	source: "src/index.ts",
 	targets: ["cjs", "esm"],
@@ -66,50 +65,34 @@ function config(options = {}) {
 }
 
 //#endregion
-//#region node_modules/tsds-lib/dist/esm/lib/moduleRoot.mjs
-function moduleRoot(dir) {
-	const packagePath = path.default.join(dir, "package.json");
-	if ((0, fs_exists_sync.default)(packagePath) && JSON.parse(fs.default.readFileSync(packagePath, "utf8")).name) return dir;
-	const nextDir = path.default.dirname(dir);
-	if (nextDir === dir) throw new Error("Package root not found");
-	return moduleRoot(nextDir);
-}
-
-//#endregion
-//#region node_modules/tsds-lib/dist/esm/lib/spawn.mjs
-const __dirname$3 = path.default.dirname(typeof __filename !== "undefined" ? __filename : url.default.fileURLToPath(require("url").pathToFileURL(__filename).href));
-const root$1 = moduleRoot(__dirname$3);
+//#region ../../packages/tsds-lib/dist/esm/lib/spawn.mjs
+const __dirname$2 = path.default.dirname(typeof __filename !== "undefined" ? __filename : url.default.fileURLToPath(require("url").pathToFileURL(__filename).href));
+const root = (0, module_root_sync.default)(__dirname$2);
 function spawn(cmd, args, options, cb) {
 	const cwd = options.cwd || process.cwd();
-	const PATH_KEY = (0, env_path_key.default)(options);
-	const env = {
-		...process.env,
-		env: options.env || {}
-	};
-	env[PATH_KEY] = (0, path_string_prepend.default)(env[PATH_KEY] || "", path.default.resolve(root$1, "..", "..", "node_modules", ".bin"));
-	env[PATH_KEY] = (0, path_string_prepend.default)(env[PATH_KEY] || "", path.default.resolve(cwd, "node_modules", ".bin"));
+	const env = options.env || process.env;
+	const pathKey = (0, env_path_key.default)(env) || "";
+	let envPath = env[pathKey] || "";
+	envPath = (0, path_string_prepend.default)(envPath, path.default.resolve(cwd, "node_modules", ".bin"));
+	envPath = (0, path_string_prepend.default)(envPath, path.default.resolve(root, "..", ".bin"));
 	(0, cross_spawn_cb.default)(cmd, args, {
 		stdio: "inherit",
 		cwd,
-		env
+		env: {
+			...env,
+			[pathKey]: envPath
+		}
 	}, cb);
 }
 
 //#endregion
-//#region node_modules/tsds-lib/dist/esm/lib/which.mjs
-const __dirname$2 = path.default.dirname(typeof __filename !== "undefined" ? __filename : url.default.fileURLToPath(require("url").pathToFileURL(__filename).href));
-const root = moduleRoot(__dirname$2);
+//#region ../../packages/tsds-lib/dist/esm/lib/lazy.cjs
+var require_lazy = __commonJS({ "../../packages/tsds-lib/dist/esm/lib/lazy.cjs"(exports, module) {
+	module.exports = require("lazy-cache")(require);
+} });
 
 //#endregion
-//#region node_modules/tsds-lib/dist/esm/lib/lazy.cjs
-var require_lazy = __commonJS({
-	"node_modules/tsds-lib/dist/esm/lib/lazy.cjs"(exports, module) {
-		module.exports = require("lazy-cache")(require);
-	}
-});
-
-//#endregion
-//#region node_modules/tsds-lib/dist/esm/lib/wrapWorker.mjs
+//#region ../../packages/tsds-lib/dist/esm/lib/wrapWorker.mjs
 var import_lazy = __toESM(require_lazy(), 1);
 const call = (0, import_lazy.default)("node-version-call");
 function wrapWorker(workerPath) {
@@ -128,7 +111,7 @@ function wrapWorker(workerPath) {
 }
 
 //#endregion
-//#region node_modules/tsds-build/dist/esm/lib/code.mjs
+//#region ../../packages/tsds-build/dist/esm/lib/code.mjs
 const MAX_FILES$1 = 10;
 function transform(_args, type$1, options, cb) {
 	const cwd = options.cwd || process.cwd();
@@ -142,7 +125,7 @@ function transform(_args, type$1, options, cb) {
 		sourceMaps: true
 	}, (err, results) => {
 		if (err) console.log(`${type$1} failed: ${err.message} from ${src}`);
-		else console.log(`Created ${results.length < MAX_FILES$1 ? results.map((x) => `dist/${type$1}${x.to}`).join(",") : `${results.length} files in dist/${type$1}`}`);
+else console.log(`Created ${results.length < MAX_FILES$1 ? results.map((x) => `dist/${type$1}${x.to}`).join(",") : `${results.length} files in dist/${type$1}`}`);
 		cb$1(err);
 	}));
 	queue.defer(fs.default.writeFile.bind(null, path.default.join(dest, "package.json"), "{\"type\":\"commonjs\"}"));
@@ -150,7 +133,7 @@ function transform(_args, type$1, options, cb) {
 }
 
 //#endregion
-//#region node_modules/tsds-build/dist/esm/lib/types.mjs
+//#region ../../packages/tsds-build/dist/esm/lib/types.mjs
 const MAX_FILES = 10;
 const type = "types";
 function cjs(_args, options, cb) {
@@ -161,24 +144,24 @@ function cjs(_args, options, cb) {
 	queue.defer((cb$1) => (0, rimraf2.default)(dest, { disableGlob: true }, cb$1.bind(null, null)));
 	queue.defer((cb$1) => (0, ts_swc_transform.transformTypes)(src, dest, (err, results) => {
 		if (err) console.log(`${type} failed: ${err.message} from ${src}`);
-		else console.log(`Created ${results.length < MAX_FILES ? results.map((x) => `dist/${type}${x.to}`).join(",") : `${results.length} files in dist/${type}`}`);
+else console.log(`Created ${results.length < MAX_FILES ? results.map((x) => `dist/${type}${x.to}`).join(",") : `${results.length} files in dist/${type}`}`);
 		cb$1(err);
 	}));
 	queue.await(cb);
 }
 
 //#endregion
-//#region node_modules/tsds-build/dist/esm/lib/umd.mjs
+//#region ../../packages/tsds-build/dist/esm/lib/umd.mjs
 const __dirname$1 = path.default.dirname(typeof __filename !== "undefined" ? __filename : url.default.fileURLToPath(require("url").pathToFileURL(__filename).href));
 const major = +process.versions.node.split(".")[0];
-const workerWrapper = wrapWorker(path.default.join(moduleRoot(__dirname$1), "dist", "cjs", "build", "umd.js"));
+const workerWrapper = wrapWorker(path.default.join((0, module_root_sync.default)(__dirname$1), "dist", "cjs", "build", "umd.js"));
 function worker(_args, options, cb) {
 	const cwd = options.cwd || process.cwd();
 	const dest = path.default.join(cwd, "dist", "umd");
 	const queue = new queue_cb.default(1);
 	queue.defer((cb$1) => (0, rimraf2.default)(dest, { disableGlob: true }, cb$1.bind(null, null)));
 	(() => {
-		const configPath = path.default.join(moduleRoot(__dirname$1), "dist", "esm", "rollup", "config.mjs");
+		const configPath = path.default.join((0, module_root_sync.default)(__dirname$1), "dist", "esm", "rollup", "config.mjs");
 		const args = [
 			"rollup",
 			"--config",
@@ -187,7 +170,7 @@ function worker(_args, options, cb) {
 		queue.defer(spawn.bind(null, args[0], args.slice(1), { cwd }));
 	})();
 	(() => {
-		const configPath = path.default.join(moduleRoot(__dirname$1), "dist", "esm", "rollup", "config.min.mjs");
+		const configPath = path.default.join((0, module_root_sync.default)(__dirname$1), "dist", "esm", "rollup", "config.min.mjs");
 		const args = [
 			"rollup",
 			"--config",
@@ -203,7 +186,7 @@ function umd(args, options, cb) {
 }
 
 //#endregion
-//#region node_modules/tsds-build/dist/esm/command.mjs
+//#region ../../packages/tsds-build/dist/esm/command.mjs
 function build(args, options, cb) {
 	const cwd = options.cwd || process.cwd();
 	const { targets } = config(options);
