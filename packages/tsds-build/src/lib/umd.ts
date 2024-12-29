36c07c279b4609cdfa5a04f269c80876f2f81679
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-29 03:14:02 -0800

    Refactor tsds-build UMD generation and update dependencies

--- packages/tsds-build/src/lib/umd.ts
+++ packages/tsds-build/src/lib/umd.ts
@@ -14,19 +14,12 @@ const workerWrapper = wrapWorker(path.join(moduleRoot(__dirname), 'dist', 'cjs',
 function worker(_args, options, cb) {
   const cwd = options.cwd || process.cwd();
   const dest = path.join(cwd, 'dist', 'umd');
+  const configRoot = path.join(moduleRoot(__dirname), 'dist', 'esm', 'rollup');
 
   const queue = new Queue(1);
   queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
-  (() => {
-    const configPath = path.join(moduleRoot(__dirname), 'dist', 'esm', 'rollup', 'config.mjs');
-    const args = ['rollup', '--config', configPath];
-    queue.defer(spawn.bind(null, args[0], args.slice(1), options));
-  })();
-  (() => {
-    const configPath = path.join(moduleRoot(__dirname), 'dist', 'esm', 'rollup', 'config.min.mjs');
-    const args = ['rollup', '--config', configPath];
-    queue.defer(spawn.bind(null, args[0], args.slice(1), options));
-  })();
+  queue.defer(spawn.bind(null, 'rollup', ['--config', path.join(configRoot, 'config.mjs')], options));
+  queue.defer(spawn.bind(null, 'rollup', ['--config', path.join(configRoot, 'config.min.mjs')], options));
   queue.defer(fs.writeFile.bind(null, path.join(dest, 'package.json'), '{"type":"commonjs"}'));
   queue.await(cb);
 }
