4a97cc4990ff301d6df44079f6ef2e24f3f7e66c
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-06 22:12:02 -0800

    Ensure bindings in tsds-build

--- packages/tsds-lib/src/lib/wrapWorker.ts
+++ packages/tsds-lib/src/lib/wrapWorker.ts
@@ -1,20 +1,14 @@
 import Module from 'module';
-import lazy from 'lazy-cache';
 
 const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
-const callLazy = lazy(_require)('node-version-call');
 
 export default function wrapWorker(workerPath: string) {
-  const workerLazy = lazy(_require)(workerPath);
-
-  return function workerWrapper(version) {
-    // biome-ignore lint/style/noArguments: <explanation>
-    const args = Array.prototype.slice.call(arguments, 1);
-    if (version === 'local') return workerLazy().apply(null, args);
+  return function workerWrapper(version, ...args) {
+    if (version === 'local') return _require(workerPath).apply(null, args);
 
     const callback = args.pop();
     try {
-      callback(null, callLazy().apply(null, [{ version, callbacks: true }, workerPath].concat(args)));
+      callback(null, _require('node-version-call').apply(null, [{ version, callbacks: true }, workerPath].concat(args)));
     } catch (err) {
       callback(err);
     }
