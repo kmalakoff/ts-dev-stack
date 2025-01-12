3290453f23c2c7a33b995387dca61af7df081142
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-12 04:30:18 -0800

    Refactor build system and introduce tsds-publish and tsds-validate

--- packages/tsds-validate/src/command.ts
+++ packages/tsds-validate/src/command.ts
@@ -3,21 +3,28 @@ import url from 'url';
 import spawn from 'cross-spawn-cb';
 import Queue from 'queue-cb';
 import resolveBin from 'resolve-bin-sync';
+import format from 'tsds-biome';
+import build from 'tsds-build';
 import { wrapWorker } from 'tsds-lib';
-import validate from './validate';
+import docs from 'tsds-typedoc';
 
 const __dirname = path.dirname(typeof __filename === 'undefined' ? url.fileURLToPath(import.meta.url) : __filename);
 const major = +process.versions.node.split('.')[0];
 const version = major > 18 ? 'local' : 'stable';
 const dist = path.join(__dirname, '..', '..');
-const workerWrapper = wrapWorker(path.join(dist, 'cjs', 'commands', 'publish.cjs'));
+const workerWrapper = wrapWorker(path.join(dist, 'cjs', 'commands.cjs'));
 
-function worker(args, options, callback) {
+function worker(_args, options, callback) {
   try {
-    const np = resolveBin('np');
+    const depcheck = resolveBin('depcheck');
+    const sortPackageJSON = resolveBin('sort-package-json');
+
     const queue = new Queue(1);
-    queue.defer(validate.bind(null, [], options));
-    queue.defer(spawn.bind(null, np, args, options));
+    queue.defer(spawn.bind(null, depcheck, [], options));
+    queue.defer(format.bind(null, [], options));
+    queue.defer(spawn.bind(null, sortPackageJSON, [], options));
+    queue.defer(docs.bind(null, [], options));
+    queue.defer(build.bind(null, [], options));
     queue.await(callback);
   } catch (err) {
     console.log(err.message);
