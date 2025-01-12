367c44f80c71376793834e8aebd8fba660bffc93
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-11 17:11:34 -0800

    Upgrade modules

--- packages/tsds-build/src/command.ts
+++ packages/tsds-build/src/command.ts
@@ -1,14 +1,20 @@
 import path from 'path';
 import Queue from 'queue-cb';
 import rimraf2 from 'rimraf2';
+import { DEFAULT_TARGETS } from './constants';
 import code from './lib/code';
 import types from './lib/types';
 import umd from './lib/umd';
-import { config } from './vendor/tsds-lib/index';
+import { loadConfig } from './vendor/tsds-lib/index';
 
-export default function build(args, options, cb) {
+export default function build(args, options, callback) {
+  const config = loadConfig(options);
+  if (!config) {
+    console.log('tsds: no config. Skipping');
+    return callback();
+  }
   const cwd = options.cwd || process.cwd();
-  const { targets } = config(options);
+  const targets = config.targets || DEFAULT_TARGETS;
   const clean = options.clean === undefined ? true : options.clean;
   const dest = path.join(cwd, 'dist');
   const queue = new Queue(1);
@@ -17,5 +23,5 @@ export default function build(args, options, cb) {
   targets.indexOf('esm') < 0 || queue.defer(code.bind(null, args, 'esm', options));
   targets.indexOf('umd') < 0 || queue.defer(umd.bind(null, args, options));
   queue.defer(types.bind(null, args, options));
-  queue.await(cb);
+  queue.await(callback);
 }
