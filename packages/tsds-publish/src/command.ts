25ea213ae2a05b36964c399f2aadcf5eff37ead7
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-12 20:43:43 -0800

    Publish publish

--- packages/tsds-publish/src/command.ts
+++ packages/tsds-publish/src/command.ts
@@ -5,8 +5,9 @@ import spawn from 'cross-spawn-cb';
 import getopts from 'getopts-compat';
 import Queue from 'queue-cb';
 import { wrapWorker } from 'tsds-lib';
-import validate from 'tsds-validate';
-import hasChanged from './hasChanged';
+import hasChanged from './lib/hasChanged';
+import post from './post';
+import pre from './pre';
 
 const __dirname = path.dirname(typeof __filename === 'undefined' ? url.fileURLToPath(import.meta.url) : __filename);
 const major = +process.versions.node.split('.')[0];
@@ -23,35 +24,36 @@ function worker(args, options, callback) {
     return callback();
   }
 
-  const opts = getopts(args, { stopEarly: true, alias: { otp: 'o' }, boolean: ['yolo'] });
+  const opts = getopts(args, { alias: { otp: 'o' }, boolean: ['yolo'] });
 
   const queue = new Queue(1);
-  let _skip = false;
 
-  opts.yolo || queue.defer(validate.bind(null, [], options));
+  opts.yolo || queue.defer(pre.bind(null, args, options));
   queue.defer((cb) => {
     hasChanged(options, (err, changed) => {
       if (err) return cb(err);
       if (!changed) {
-        _skip = true;
         console.log(`Skipping ${options.package.name}. No changes`);
         return cb();
       }
 
-      const versionArgs = ['version', opts._.length >= 0 ? opts._[0] : 'patch'];
-      queue.defer((cb) => spawn('npm', versionArgs, options, (err) => { 
-        if (err) return cb(err);
-        options.package = JSON.parse(fs.readFileSync(path.join(cwd, 'package.json'), 'utf8'));
-        cb();
-      }));
+      // update the version
+      const versionArgs = ['version', opts._.length > 0 ? opts._[0] : 'patch'];
+      queue.defer((cb) =>
+        spawn('npm', versionArgs, options, (err) => {
+          if (err) return cb(err);
+          options.package = JSON.parse(fs.readFileSync(path.join(cwd, 'package.json'), 'utf8'));
+          cb();
+        })
+      );
 
+      // do publish
       const publishArgs = ['publish'];
       if (opts.otp) Array.prototype.push.apply(publishArgs, [`--otp=${opts.otp}`]);
       queue.defer(spawn.bind(null, 'npm', publishArgs, options));
 
-      queue.defer(spawn.bind(null, 'git', ['add', '.'], options));
-      queue.defer(spawn.bind(null, 'git', ['commit', '-m', `"v${options.package.version}"`], options));
-      queue.defer(spawn.bind(null, 'git', ['tag', '-a', `"v${options.package.version}"`, `-m"v${options.package.version}"`], options));
+      // add post actions
+      queue.defer(post.bind(null, args, options));
       cb();
     });
   });
---
+++ packages/tsds-publish/src/post.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+import spawn from 'cross-spawn-cb';
+import Queue from 'queue-cb';
+import { wrapWorker } from 'tsds-lib';
+
+const __dirname = path.dirname(typeof __filename === 'undefined' ? url.fileURLToPath(import.meta.url) : __filename);
+const major = +process.versions.node.split('.')[0];
+const version = major > 18 ? 'local' : 'stable';
+const dist = path.join(__dirname, '..', '..');
+const workerWrapper = wrapWorker(path.join(dist, 'cjs', 'commands.cjs'));
+
+function worker(_args, options, callback) {
+  const cwd = options.cwd || process.cwd();
+  options.package = JSON.parse(fs.readFileSync(path.join(cwd, 'package.json'), 'utf8'));
+
+  const queue = new Queue(1);
+  if ((options.package.scripts || {}).version) queue.defer(spawn.bind(null, 'npm', ['run', 'version'], { ...options, cwd }));
+  queue.defer((cb) => spawn('git', ['add', '.'], options, cb.bind(null, null)));
+  fs.stat(path.join(cwd, '.git'), (err) => {
+    // root with git
+    if (!err) {
+      queue.defer((cb) => spawn('git', ['commit', '-m', `"v${options.package.version}"`], options, cb.bind(null, null)));
+      queue.defer((cb) => spawn('git', ['tag', '-a', `"v${options.package.version}"`, `-m"v${options.package.version}"`], options, cb.bind(null, null)));
+    }
+    queue.await(callback);
+  });
+}
+
+export default function post(args, options, callback) {
+  major < 0 ? workerWrapper(version, args, options, callback) : worker(args, options, callback);
+}
