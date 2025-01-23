9abd8699cf157e2339e1c9579a6dc55ccf9d8a00
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-23 13:13:43 -0800

    Add ECONNRESET to install retry errors

--- packages/ts-dev-stack/src/lib/concatWritable.ts
+++ packages/ts-dev-stack/src/lib/concatWritable.ts
@@ -1,12 +1,12 @@
 import { Writable } from 'readable-stream';
 export default function concatWritable(callback) {
-    const chunks = [];
-    const stream = new Writable({
-        write: (chunk, _encoding, next)=>{
-            chunks.push(chunk);
-            next();
-        }
-    });
-    stream.on('finish', ()=>callback(Buffer.concat(chunks.splice(0))));
-    return stream;
+  const chunks = [];
+  const stream = new Writable({
+    write: (chunk, _encoding, next) => {
+      chunks.push(chunk);
+      next();
+    },
+  });
+  stream.on('finish', () => callback(Buffer.concat(chunks.splice(0))));
+  return stream;
 }
