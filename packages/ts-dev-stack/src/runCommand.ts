5829fcc44ca20be0f687b15d2a9354f1a76c1672
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-23 13:13:43 -0800

    Upgrade modules

--- packages/ts-dev-stack/src/runCommand.ts
+++ packages/ts-dev-stack/src/runCommand.ts
@@ -14,42 +14,38 @@ const nodeModules = path.join(_dirname, '..', '..', 'node_modules');
 const moduleRegEx = /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/;
 
 function run(specifier, args, options, callback) {
-    try {
-        const mod = _require(specifier);
-        const fn = mod.default || mod;
-        fn(args, options, callback);
-    } catch (err) {
-        return callback(err);
-    }
+  try {
+    const mod = _require(specifier);
+    const fn = mod.default || mod;
+    fn(args, options, callback);
+  } catch (err) {
+    return callback(err);
+  }
 }
 
 export default function runCommand(name, args, options, callback) {
-    const config = loadConfig(options);
-    if (!config) {
-        console.log(`tsds: config missing source. Skipping command: ${name}`);
-        return callback();
+  const config = loadConfig(options);
+  const commands = {
+    ...constants.commands,
+    ...((config || {}).commands || {}),
+  };
+  const command = commands[name];
+  if (!command) return callback(new Error(`Unrecognized command: ${name} ${args.join(' ')}`));
+  const { _, ...opts } = getopts(args, { stopEarly: true, alias: { 'dry-run': 'dr' }, boolean: ['dry-run'] });
+  if (opts['dry-run']) return callback();
+  const cwd = options.cwd || process.cwd();
+  const runOptions = { ...options, cwd, stdio: 'inherit' };
+  if (moduleRegEx.test(command)) {
+    try {
+      resolve.sync(path.join(command, 'package.json'));
+      return run(command, args, runOptions, callback);
+    } catch (_err) {
+      return installModule(command, nodeModules, (err) => {
+        console.log(`Module missing: ${command}. ${err ? `Failed install: ${err.message}` : 'Installed'}`);
+        err ? callback(err) : run(command, args, runOptions, callback);
+      });
     }
-    const commands = {
-        ...constants.commands,
-        ...config.commands || {}
-    };
-    const command = commands[name];
-    if (!command) return callback(new Error(`Unrecognized command: ${name} ${args.join(' ')}`));
-    const { _, ...opts } = getopts(args, { stopEarly: true, alias: { 'dry-run': 'dr' }, boolean: [ 'dry-run' ]});
-    if (opts['dry-run']) return callback();
-    const cwd = options.cwd || process.cwd();
-    const runOptions = { ...options, cwd, stdio: 'inherit' };
-    if (moduleRegEx.test(command)) {
-        try {
-            resolve.sync(path.join(command, 'package.json'));
-            return run(command, args, runOptions, callback);
-        } catch (_err) {
-            return installModule(command, nodeModules, (err)=>{
-                console.log(`Module missing: ${command}. ${err ? `Failed install: ${err.message}` : 'Installed'}`);
-                err ? callback(err) : run(command, args, runOptions, callback);
-            });
-        }
-    }
-    // for relative files, ensure the extension matches
-    return run(path.join(dist, 'cjs', command), args, runOptions, callback);
+  }
+  // for relative files, ensure the extension matches
+  return run(path.join(dist, 'cjs', command), args, runOptions, callback);
 }
