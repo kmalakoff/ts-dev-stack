3d7ca18795493b78b077d1fdb729dcd6a7e122a3
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-22 23:01:56 -0800

    Upgrade modules

--- packages/ts-dev-stack/src/commands/link.ts
+++ packages/ts-dev-stack/src/commands/link.ts
@@ -1,7 +1,7 @@
 import { link } from 'link-unlink';
 import { installPath } from 'tsds-lib';
 
-export default function linkCmd(_args, options, cb) {
-  const cwd = options.cwd || process.cwd();
-  link(cwd, installPath(options), cb);
+export default function command(_args, options, callback) {
+    const cwd = options.cwd || process.cwd();
+    link(cwd, installPath(options), callback);
 }
