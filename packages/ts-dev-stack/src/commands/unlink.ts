0407691dd2f0e3d8f9da9e6d320e0905bc728caa
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-22 23:01:56 -0800

    Add install command and update dependencies

--- packages/ts-dev-stack/src/commands/unlink.ts
+++ packages/ts-dev-stack/src/commands/unlink.ts
@@ -1,6 +1,6 @@
 import { unlink } from 'link-unlink';
 import { installPath } from 'tsds-lib';
 
-export default function unlinkCmd(_args, options, cb) {
-  unlink(installPath(options), cb);
+export default function command(_args, options, callback) {
+    unlink(installPath(options), callback);
 }
