c4a1c0d7cb647d171e918dc2d748f8ea77392f00
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-23 13:13:43 -0800

    Upgrade modules

--- packages/tsds-publish/src/lib/hasChanged.ts
+++ packages/tsds-publish/src/lib/hasChanged.ts
@@ -23,35 +23,35 @@ interface Registry {
 }
 
 async function worker(options, callback) {
-    const cwd = options.cwd || process.cwd();
-    options = {
-        ...options
-    };
-    options.package = options.package || JSON.parse(fs.readFileSync(path.join(cwd, 'package.json'), 'utf8'));
-    try {
-        const Arborist = _require('@npmcli/arborist');
-        const npa = _require('npm-package-arg');
-        const pacote = _require('pacote');
-        const res = await get(`https://registry.npmjs.org/${options.package.name}`).json();
-        const integrity = (res.body as Registry).versions[(res.body['dist-tags'] as DistTag).latest].dist.integrity;
-        const spec = npa(cwd);
-        const manifest = await pacote.manifest(spec, {
-            Arborist
-        });
-        const tarball = await pacote.tarball(manifest._resolved, {
-            Arborist,
-            integrity: manifest._integrity
-        });
-        const parts = integrity.split('-');
-        const algorithm = parts.shift();
-        const latest = parts.join('-');
-        const local = crypto.createHash(algorithm).update(new Uint8Array(tarball)).digest('base64');
-        callback(null, latest !== local);
-    } catch (err) {
-        callback(err, true);
-    }
+  const cwd = options.cwd || process.cwd();
+  options = {
+    ...options,
+  };
+  options.package = options.package || JSON.parse(fs.readFileSync(path.join(cwd, 'package.json'), 'utf8'));
+  try {
+    const Arborist = _require('@npmcli/arborist');
+    const npa = _require('npm-package-arg');
+    const pacote = _require('pacote');
+    const res = await get(`https://registry.npmjs.org/${options.package.name}`).json();
+    const integrity = (res.body as Registry).versions[(res.body['dist-tags'] as DistTag).latest].dist.integrity;
+    const spec = npa(cwd);
+    const manifest = await pacote.manifest(spec, {
+      Arborist,
+    });
+    const tarball = await pacote.tarball(manifest._resolved, {
+      Arborist,
+      integrity: manifest._integrity,
+    });
+    const parts = integrity.split('-');
+    const algorithm = parts.shift();
+    const latest = parts.join('-');
+    const local = crypto.createHash(algorithm).update(new Uint8Array(tarball)).digest('base64');
+    callback(null, latest !== local);
+  } catch (err) {
+    callback(err, true);
+  }
 }
 
 export default function hasChanged(options, callback) {
-    version !== 'local' ? workerWrapper(version, options, callback) : worker(options, callback);
+  version !== 'local' ? workerWrapper(version, options, callback) : worker(options, callback);
 }
