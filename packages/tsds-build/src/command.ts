ac60c5245280cbae4f1c770436beecf2b159c77d
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-19 16:40:43 -0700

    1.15.17

--- packages/tsds-build/src/command.ts
+++ packages/tsds-build/src/command.ts
@@ -2,10 +2,9 @@ import path from 'path';
 import Queue from 'queue-cb';
 import rimraf2 from 'rimraf2';
 import { loadConfig } from 'tsds-lib';
-import { DEFAULT_TARGETS } from './constants';
-import code from './lib/code';
-import types from './lib/types';
-import umd from './lib/umd';
+import { DEFAULT_TARGETS } from './constants.js';
+import files from './lib/files.js';
+import umd from './lib/umd.js';
 
 export default function build(args, options, callback) {
   const config = loadConfig(options);
@@ -19,9 +18,8 @@ export default function build(args, options, callback) {
   const dest = path.join(cwd, 'dist');
   const queue = new Queue(1);
   !clean || queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
-  targets.indexOf('cjs') < 0 || queue.defer(code.bind(null, args, 'cjs', options));
-  targets.indexOf('esm') < 0 || queue.defer(code.bind(null, args, 'esm', options));
+  targets.indexOf('cjs') < 0 || queue.defer(files.bind(null, args, 'cjs', options));
+  targets.indexOf('esm') < 0 || queue.defer(files.bind(null, args, 'esm', options));
   targets.indexOf('umd') < 0 || queue.defer(umd.bind(null, args, options));
-  queue.defer(types.bind(null, args, options));
   queue.await(callback);
 }
