50d6fda1226dc24a35644119f62ba6ff8721d82c
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-17 23:33:24 -0700

    Lock mocha to 11.6.0

--- packages/tsds-lib/src/index.ts
+++ packages/tsds-lib/src/index.ts
@@ -1,4 +1,4 @@
-export * from './types.js';
-export { default as loadConfig } from './lib/loadConfig.js';
 export { default as installPath } from './lib/installPath.js';
+export { default as loadConfig } from './lib/loadConfig.js';
 export { default as wrapWorker } from './lib/wrapWorker.js';
+export * from './types.js';
