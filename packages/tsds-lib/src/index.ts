a96c6c55d24a3d6295a072fdc0c932558d9230e8
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-20 21:41:03 -0700

    rewriteRelativeImportExtensions

--- packages/tsds-lib/src/index.ts
+++ packages/tsds-lib/src/index.ts
@@ -1,4 +1,4 @@
-export { default as installPath } from './lib/installPath.js';
-export { default as loadConfig } from './lib/loadConfig.js';
-export { default as wrapWorker } from './lib/wrapWorker.js';
-export * from './types.js';
+export { default as installPath } from './lib/installPath.ts';
+export { default as loadConfig } from './lib/loadConfig.ts';
+export { default as wrapWorker } from './lib/wrapWorker.ts';
+export * from './types.ts';
