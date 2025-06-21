ad6557fa78acd4733e0df07d413478fa24a482f3
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 14:13:04 -0700

    1.17.3

--- packages/ts-dev-stack/src/commands/link.ts
+++ packages/ts-dev-stack/src/commands/link.ts
@@ -2,7 +2,7 @@ import { link } from 'link-unlink';
 import type { CommandCallback, CommandOptions } from 'tsds-lib';
 import { installPath } from 'tsds-lib';
 
-export default function command(_args: string[], options: CommandOptions, callback: CommandCallback) {
+export default function command(_args: string[], options: CommandOptions, callback: CommandCallback): undefined {
   const cwd: string = (options.cwd as string) || process.cwd();
   link(cwd, installPath(options), callback);
 }
