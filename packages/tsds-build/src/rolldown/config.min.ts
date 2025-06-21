055f0b5502f8f2ccbadf30a633a6204a70e5c0b8
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 13:33:48 -0700

    1.17.2

--- packages/tsds-build/src/rolldown/config.min.ts
+++ packages/tsds-build/src/rolldown/config.min.ts
@@ -1,6 +1,6 @@
 import camelcase from 'lodash.camelcase';
 import path from 'path';
-import { defineConfig, type Plugin } from 'rolldown';
+import { defineConfig, type RolldownOptions } from 'rolldown';
 import swc from 'ts-swc-rollup-plugin';
 
 import { globals, input, pkg, tsconfig } from '../lib/rollingOptions.ts';
@@ -14,16 +14,16 @@ fs.writeFileSync(`${tsconfig.path}.es5`, JSON.stringify(tsconfig.config), 'utf8'
 export default defineConfig({
   input,
   output: {
-    file: path.join(process.cwd(), 'dist', 'umd', `${pkg.name}.min.cjs`),
+    file: path.join(process.cwd(), 'dist', 'umd', `${pkg.name}.min.cjs`) as string,
     format: 'umd',
     sourcemap: true,
-    name: camelcase(pkg.name),
+    name: camelcase(pkg.name) as string,
     globals,
     minify: true,
   },
   external: (module) => !!moduleRegEx.test(module),
-  plugins: [swc({ tsconfig }) as unknown as Plugin],
+  plugins: [swc({ tsconfig })],
   resolve: {
     tsconfigFilename: `${tsconfig.path}.es5`,
   },
-});
+}) as RolldownOptions;
