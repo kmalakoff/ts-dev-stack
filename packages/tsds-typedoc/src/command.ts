97792e431209f779c622bd81d8495c855a742861
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-21 19:34:28 -0700

    Link latest package version during tsds-typedoc worker execution

--- packages/tsds-typedoc/src/command.ts
+++ packages/tsds-typedoc/src/command.ts
@@ -1,10 +1,11 @@
 import spawn from 'cross-spawn-cb';
+import { link, unlink } from 'link-unlink';
 import mkdirp from 'mkdirp-classic';
 import path from 'path';
 import Queue from 'queue-cb';
 import resolveBin from 'resolve-bin-sync';
 import rimraf2 from 'rimraf2';
-import { type CommandCallback, type CommandOptions, loadConfig, wrapWorker } from 'tsds-lib';
+import { type CommandCallback, type CommandOptions, installPath, loadConfig, wrapWorker } from 'tsds-lib';
 import url from 'url';
 
 const major = +process.versions.node.split('.')[0];
@@ -25,14 +26,21 @@ function worker(args: string[], options: CommandOptions, callback: CommandCallba
   }
 
   try {
+    const cwd: string = (options.cwd as string) || process.cwd();
     const typedoc = resolveBin('typedoc');
-    const dest = path.join(process.cwd(), 'docs');
+    const dest = path.join(cwd, 'docs');
 
     const queue = new Queue(1);
     queue.defer((cb) => rimraf2(dest, { disableGlob: true }, cb.bind(null, null)));
     queue.defer(mkdirp.bind(null, dest));
+    queue.defer(link.bind(null, cwd, installPath(options))); // link the latest for tests
     queue.defer(spawn.bind(null, typedoc, [config.source, ...args, '--excludeExternals', 'true'], options));
-    queue.await(callback);
+    queue.await((err) => {
+      // unlink the latest for tests
+      unlink(installPath(options), () => {
+        callback(err);
+      });
+    });
   } catch (err) {
     return callback(err);
   }
