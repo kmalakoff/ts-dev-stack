56599aca3b3e69bb584503e758b70272ac70d57c
Kevin Malakoff <kmalakoff@gmail.com>
2025-06-30 07:52:34 -0700

    Bump version to 1.18.0 and refactor tsds-publish command logic

--- packages/tsds-publish/src/command.ts
+++ packages/tsds-publish/src/command.ts
@@ -3,11 +3,10 @@ import fs from 'fs';
 import getopts from 'getopts-compat';
 import path from 'path';
 import Queue from 'queue-cb';
+import rimraf2 from 'rimraf2';
 import { type CommandCallback, type CommandOptions, wrapWorker } from 'tsds-lib';
 import url from 'url';
 import hasChanged from './lib/hasChanged.ts';
-import post from './post.ts';
-import pre from './pre.ts';
 
 const major = +process.versions.node.split('.')[0];
 const version = major > 14 ? 'local' : 'stable';
@@ -24,43 +23,48 @@ function worker(args: string[], options_: CommandOptions, callback: CommandCallb
     return callback();
   }
 
-  const queue = new Queue(1);
-
   const opts = getopts(args, { alias: { otp: 'o' }, boolean: ['yolo'] });
-  opts.yolo || queue.defer(pre.bind(null, args, options));
-  queue.defer((cb) => {
-    hasChanged(options, (err, changed): undefined => {
-      if (err) {
-        cb(err);
-        return;
-      }
-      if (!changed) {
-        console.log(`Skipping ${options.package.name}. No changes`);
+  hasChanged(options, (err, changed): undefined => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    if (!changed) {
+      console.log(`Skipping ${options.package.name}. No changes`);
+      callback();
+      return;
+    }
+
+    const queue = new Queue(1);
+
+    // run tests
+    if (!opts.yolo) {
+      queue.defer(rimraf2.bind(null, path.join(cwd as string, 'node_modules'), { disableGlob: true }));
+      queue.defer(spawn.bind(null, 'npm', ['ci'], { ...options, cwd }));
+      queue.defer(spawn.bind(null, 'npm', ['test'], { ...options, cwd }));
+    }
+
+    // update the version
+    const versionArgs = ['version', opts._.length > 0 ? opts._[0] : 'patch'];
+    queue.defer((cb) =>
+      spawn('npm', versionArgs, options, (err) => {
+        if (err) return cb(err);
+        options.package = JSON.parse(fs.readFileSync(path.join(cwd as string, 'package.json'), 'utf8'));
         cb();
-        return;
-      }
+      })
+    );
 
-      // update the version
-      const versionArgs = ['version', opts._.length > 0 ? opts._[0] : 'patch'];
-      queue.defer((cb) =>
-        spawn('npm', versionArgs, options, (err) => {
-          if (err) return cb(err);
-          options.package = JSON.parse(fs.readFileSync(path.join(cwd as string, 'package.json'), 'utf8'));
-          cb();
-        })
-      );
+    // do publish
+    const publishArgs = ['publish'];
+    if (opts.otp) Array.prototype.push.apply(publishArgs, [`--otp=${opts.otp}`]);
+    queue.defer(spawn.bind(null, 'npm', publishArgs, options));
 
-      // do publish
-      const publishArgs = ['publish'];
-      if (opts.otp) Array.prototype.push.apply(publishArgs, [`--otp=${opts.otp}`]);
-      queue.defer(spawn.bind(null, 'npm', publishArgs, options));
-
-      // add post actions
-      queue.defer(post.bind(null, args, options));
-      cb();
-    });
+    // do post actions
+    if ((options.package.scripts || {}).version) queue.defer(spawn.bind(null, 'npm', ['run', 'version'], { ...options, cwd }));
+    queue.defer((cb) => spawn('git', ['add', '.'], options, cb.bind(null, null)));
+    queue.defer((cb) => spawn('git', ['commit', '-m', `${options.package.version}`], options, cb.bind(null, null)));
+    queue.await(callback);
   });
-  queue.await(callback);
 }
 
 export default function publish(args: string[], options: CommandOptions, callback: CommandCallback): undefined {
