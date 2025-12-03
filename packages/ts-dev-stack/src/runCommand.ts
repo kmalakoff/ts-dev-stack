43608f51a01b54da437be8f29fc4409d1deee607
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-02 22:11:40 -0800

    Refactor

--- packages/ts-dev-stack/src/runCommand.ts
+++ packages/ts-dev-stack/src/runCommand.ts
@@ -11,9 +11,7 @@ const resolveSync = (resolve.default ?? resolve).sync;
 
 const _require = typeof require === 'undefined' ? Module.createRequire(import.meta.url) : require;
 const _dirname = path.dirname(typeof __filename === 'undefined' ? url.fileURLToPath(import.meta.url) : __filename);
-const dist = path.join(_dirname, '..');
 const nodeModules = path.join(_dirname, '..', '..', 'node_modules');
-const moduleRegEx = /^[^./]|^\.[^./]|^\.\.[^/]/;
 
 function run(specifier: string, args: string[], options: CommandOptions, callback: CommandCallback): undefined {
   try {
@@ -37,17 +35,13 @@ export default function runCommand(name: string, args: string[], options: Comman
   if (opts['dry-run']) return callback();
   const cwd: string = (options.cwd as string) || process.cwd();
   const runOptions = { ...options, cwd, stdio: 'inherit' } as CommandOptions;
-  if (moduleRegEx.test(command)) {
-    try {
-      resolveSync(path.join(command, 'package.json'), { basedir: _dirname }); // pass basedir because internally resolveSync doesn't properly handle file://basedir on esm
-      return run(command, args, runOptions, callback);
-    } catch (_err) {
-      installModule(command, nodeModules, (err) => {
-        console.log(`Module missing: ${command}. ${err ? `Failed install: ${err.message}` : 'Installed'}`);
-        err ? callback(err) : run(command, args, runOptions, callback);
-      });
-    }
+  try {
+    resolveSync(path.join(command, 'package.json'), { basedir: _dirname }); // pass basedir because internally resolveSync doesn't properly handle file://basedir on esm
+    return run(command, args, runOptions, callback);
+  } catch (_err) {
+    installModule(command, nodeModules, (err) => {
+      console.log(`Module missing: ${command}. ${err ? `Failed install: ${err.message}` : 'Installed'}`);
+      err ? callback(err) : run(command, args, runOptions, callback);
+    });
   }
-  // for relative files, ensure the extension matches
-  return run(path.join(dist, 'cjs', command), args, runOptions, callback);
 }
