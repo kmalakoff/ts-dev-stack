aff34dae36cc00a8c885b57d7657de10a92015fe
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-02 22:11:40 -0800

    Refactor

--- packages/tsds-mocha/scripts/postinstall.cjs
+++ packages/tsds-mocha/scripts/postinstall.cjs
@@ -13,19 +13,19 @@ function patch(name, callback) {
     var mochaCompat = path.dirname(resolve.sync('mocha-compat/package.json'));
     var sourcePath = fs.realpathSync(path.join(mochaCompat, 'vendor', 'glob'));
 
-    var patchPath = path.dirname(resolve.sync(name + '/package.json'));
+    var patchPath = path.dirname(resolve.sync(`${name}/package.json`));
 
     var queue = new Queue();
-    queue.defer(function (callback) {
+    queue.defer((callback) => {
       var filePath = fs.realpathSync(path.join(patchPath, 'lib', 'cli', 'lookup-files.js'));
       var find = "require('glob')";
       var replace = `require('${unixify(path.relative(path.dirname(filePath), sourcePath))}')`;
 
-      fs.readFile(filePath, 'utf8', function (err, contents) {
+      fs.readFile(filePath, 'utf8', (err, contents) => {
         if (err) return callback(err);
         var newContents = contents.replace(find, replace);
         if (contents === newContents) return callback(); // no change
-        fs.writeFile(filePath, newContents, 'utf8', function (err) {
+        fs.writeFile(filePath, newContents, 'utf8', (err) => {
           if (err) return callback(err);
           console.log(`Patched glob in: ${filePath}`);
           callback();
@@ -33,12 +33,12 @@ function patch(name, callback) {
       });
     });
     name === 'mocha' ||
-      queue.defer(function (callback) {
+      queue.defer((callback) => {
         var filePath = path.join(patchPath, 'package.json');
         var pkg = require(filePath);
         if (pkg.bin[name] !== undefined) return callback();
         pkg.bin[name] = pkg.bin.mocha;
-        fs.writeFile(filePath, JSON.stringify(pkg, null, 2), 'utf8', function (err) {
+        fs.writeFile(filePath, JSON.stringify(pkg, null, 2), 'utf8', (err) => {
           if (err) return callback(err);
           console.log(`Patched bin in: ${filePath}`);
           callback();
@@ -52,12 +52,12 @@ function patch(name, callback) {
 
 // run patches
 var queue = new Queue();
-MOCHAS.forEach(function (name) {
+MOCHAS.forEach((name) => {
   queue.defer(patch.bind(null, name));
 });
-queue.await(function (err) {
+queue.await((err) => {
   if (err) {
-    console.log('postinstall failed. Error: ' + err.message);
+    console.log(`postinstall failed. Error: ${err.message}`);
     process.exit(-1);
   } else {
     console.log('postinstall succeeded');
