2b127c32e9e1439401930deb531bbb94cfa04823
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-03 08:55:20 -0800

    Upgrade modules

--- packages/tsds-publish/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { default } from './command.ts';
-export { default as hasChanged } from './lib/hasChanged.ts';
-export * from './types.ts';
--- packages/tsds-unlink/src/command.ts
+++
@@ -1,7 +0,0 @@
-import { unlink } from 'link-unlink';
-import type { CommandCallback, CommandOptions } from 'tsds-lib';
-import { installPath } from 'tsds-lib';
-
-export default function command(_args: string[], options: CommandOptions, callback: CommandCallback): undefined {
-  unlink(installPath(options), callback);
-}
