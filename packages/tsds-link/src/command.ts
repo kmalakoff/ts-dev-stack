f2284cb771308d241e61f61a0a6ae77ccbce44b5
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-03 08:55:20 -0800

    Upgrade modules

--- packages/tsds-link/src/command.ts
+++
@@ -1,8 +0,0 @@
-import { link } from 'link-unlink';
-import type { CommandCallback, CommandOptions } from 'tsds-lib';
-import { installPath } from 'tsds-lib';
-
-export default function command(_args: string[], options: CommandOptions, callback: CommandCallback): undefined {
-  const cwd: string = (options.cwd as string) || process.cwd();
-  link(cwd, installPath(options), callback);
-}
