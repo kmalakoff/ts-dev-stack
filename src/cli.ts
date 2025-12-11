ee0a829f6eb326c93d0fc6f87db96200be00fae1
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-11 10:04:32 -0800

    Upgrade modules

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,4 @@
-import exit from 'exit';
+import exit from 'exit-compat';
 import fs from 'fs';
 import getopts from 'getopts-compat';
 import path from 'path';
@@ -12,14 +12,16 @@ const ERROR_CODE = 15;
 export default function cli(argv: string[], name: string): undefined {
   if (argv.length === 0) {
     console.log(`Command missing ${name} ${argv.join(',')}`);
-    return exit(ERROR_CODE);
+    exit(ERROR_CODE);
+    return;
   }
 
   const { _, ...opts } = getopts(argv, { stopEarly: true, alias: { version: 'v' }, boolean: ['version'] });
   if (opts.version) {
     const pkg = JSON.parse(fs.readFileSync(path.join(_dirname, '..', '..', 'package.json'), 'utf8'));
     console.log(pkg.version);
-    return exit(0);
+    exit(0);
+    return;
   }
   runCommand(argv[0], argv.slice(1), {}, (err) => {
     if (err && err.message.indexOf('ExperimentalWarning') >= 0) err = null;
